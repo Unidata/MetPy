aa53d7f085fc56a17c85afe7dcec7e1b0b2e9c81
Ryan May <rmay31@gmail.com>
2008-11-10 16:20:12 +0000

    Update setup.py for rename of getobs to readers
    git-svn-id: http://code.forwarn.org/metpy/svn@28 150532fb-1d5b-0410-a8ab-efec50f980d4

--- trunk/setup.py
+++ trunk/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name = 'MetPy',
       version = '0.1',
       packages = ['metpy', 'metpy.bl', 'metpy.bl.sim', 'metpy.bl.turb',
-        'metpy.vis', 'metpy.getobs', 'metpy.mkobsnc'],
+        'metpy.vis', 'metpy.readers', 'metpy.mkobsnc'],
       platforms = ['Linux'],
       description = 'Collection of tools for reading, visualizing and'
         'performing calculations with weather data.',
