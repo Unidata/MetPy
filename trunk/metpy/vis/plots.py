637a2bdf22a6e667ea6e1d15c3a72c826daaead3
Ryan May <rmay31@gmail.com>
2008-12-03 23:06:52 +0000

    Work in progress on meteogram.  Works much more nicely now, but there's still more to be done.
    git-svn-id: http://code.forwarn.org/metpy/svn@68 150532fb-1d5b-0410-a8ab-efec50f980d4

--- trunk/metpy/vis/plots.py
+++ trunk/metpy/vis/plots.py
@@ -2,44 +2,125 @@ __all__ = ['meteogram']
 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import NullFormatter
+from matplotlib.dates import DateFormatter
 
-def meteogram(site, dt=None, **fields):
-    if dt is None:
-        import datetime
-        dt = datetime.date.today()
+#WORK IN PROGRESS
+#TODO:
+#   *linestyles should map variable names to dictionary of matplotlib
+#       keywords, plus a keyword 'fill'
+#   *implement support for mesonet style filled plots
+#   *add support for specifying units
+#   *add support for specifying data limits
+#   *figure out how to specify x-axis limits in time and allow those
+#       to be set by user
+#   *Update documentation for existing options and documentation for
+#       new options
+#Elsewhere, need a dewpoint calculation function
 
-    time = fields['time']/60.
-    temp = fields['temp']
-    relh = fields['relh']
-    press = fields['press']
-    wspd = fields['wspd']
-    
-    ax1 = plt.subplot(3,1,1)
-    plt.plot(time, relh, 'g')
-    plt.ylabel('Rel. Humidity (%)')
-    plt.xlim(0,24)
-    plt.title('Meteogram for %s on %s' % (site.upper(), dt))
-    ax1.xaxis.set_major_formatter(NullFormatter())
-    
-    ax2 = plt.subplot(3,1,2)
-    plt.plot(time, temp, 'r')
-    plt.ylabel('Temperature (C)')
-    plt.xlim(0,24)
-    ax2.xaxis.set_major_formatter(NullFormatter())
-    
-    ax3 = plt.subplot(3,1,3)
-    plt.plot(time, press, 'k')
-    plt.xlabel('Time (hour)')
-    plt.ylabel('Pressure (mb)')
-    ax32 = plt.twinx(ax3)
-    ax32.plot(time, wspd, 'b')
-    ax32.set_ylabel('Wind Speed (m/s)')
-    plt.xlim(0,24)
-    plt.xticks(range(0,25,3))
-    
-    plt.subplots_adjust(hspace=0.1)
-    
-    return [ax1, ax2, (ax3, ax32)]
+def meteogram(data, layout=None, linestyles=None, field_info=None):
+    '''
+    Plots a meteogram (collection of time series) for a data set. This
+    is broken down into a series of panels (defaults to 3), each of which
+    can plot multiple variables, with sensible defaults, but can also be
+    specified using *layout*.
+
+    *data* : numpy record array
+        A numpy record array containing time series for individual variables
+        in each field.
+
+    *layout* : dictionary
+        A dictionary that maps panel numbers to lists of variables.
+        The maximum panel number is used as the total number of panels.
+
+    *linestyles* : dictionary
+        A dictionary that maps variable names to linestyles.
+
+    *field_info* : dictionary
+        A dictionary that maps standard names, like 'temperature' or
+        'dewpoint' to the respective variables in the data record array.
+    '''
+
+    if field_info is None:
+        field_info = {}
+
+    inv_field_info = dict(zip(field_info.values(), field_info.keys()))
+
+    def map_field(name):
+        return field_info.get(name, name)
+
+    #List of variables in each panel.  None denotes that at that point, twinx
+    #should be called and the remaining variables plotted on the other axis
+    default_layout = {
+        0:[map_field('temperature'), None, map_field('relative humidity')],
+        1:[map_field('wind speed'), map_field('wind gusts'), None,
+            map_field('wind direction')],
+        2:[map_field('pressure')],
+        3:[map_field('rainfall')],
+        4:[map_field('solar radiation')]}
+
+    if layout is not None:
+        default_layout.update(layout)
+    layout = default_layout
+
+    default_linestyles = {map_field('relative humidity'):('green','fill'),
+        map_field('temperature'):('red', 'fill'),
+        map_field('pressure'):('brown', 'fill'),
+        map_field('dewpoint'):('green' 'fill'),
+        map_field('wind speed'):('blue','fill'),
+        map_field('wind gusts'):('lightblue', 'fill'),
+        map_field('wind direction'):('goldenrod', 'o'),
+        map_field('rainfall'):('lightgreen', 'fill'),
+        map_field('solar radiation'):('orange', 'fill')}
+
+    if linestyles is not None:
+        default_linestyles.update(linestyles)
+    linestyles = default_linestyles
+
+    #Get the time variable, using field info if necessary
+    dt = field_info.get('datetime', 'datetime')
+    time = data[dt]
+
+    #Get the station name, using field info if necessary
+    site_name = field_info.get('site', 'site')
+    site = data[site_name][0]
+    num_panels = max(layout.keys()) + 1
+
+    #Get the date from the first time
+    date = time[0].strftime('%y-%m-%d')
+
+    axes = []
+    for panel in range(num_panels):
+        if panel > 0:
+            ax = plt.subplot(num_panels, 1, panel+1, sharex=ax)
+        else:
+            ax = plt.subplot(num_panels, 1, panel+1)
+            ax.set_title('Meteogram for %s on %s' % (site, date))
+        for varname in layout[panel]:
+            if varname is None:
+                ax = plt.twinx(ax)
+                continue
+
+            # Get the linestyle for this variable
+            color,style = linestyles.get(varname, ('k', '-'))
+
+            #TODO This needs to be implemented
+            if style == 'fill':
+                style = '-'
+
+            #Get the variable from the data and plot
+            var = data[map_field(varname)]
+            ax.plot(time, var, style, color=color)
+
+            # Set the label to the title-cased nice-version from the
+            # field info.
+            ax.set_ylabel(inv_field_info[varname].title())
+
+        # If this is not the last panel, turn off the lower tick labels
+        ax.xaxis.set_major_formatter(DateFormatter('%H'))
+        axes.append(ax)
+        ax.set_xlabel('Time (UTC)')
+
+    return axes
 
 import numpy as np
 from numpy import ma
