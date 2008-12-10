da770cf95a2bb0004c267e679cf42c01d8c01794
Ryan May <rmay31@gmail.com>
2008-12-10 16:14:25 +0000

    Update setup.py for repo structure changes.
    git-svn-id: http://code.forwarn.org/metpy/svn@122 150532fb-1d5b-0410-a8ab-efec50f980d4

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(name = 'MetPy',
       version = '0.1',
-      packages = ['metpy', 'metpy.bl', 'metpy.bl.sim', 'metpy.bl.turb',
-        'metpy.vis', 'metpy.readers', 'metpy.mkobsnc'],
+      packages = ['metpy', 'metpy.bl', 'metpy.readers', 'metpy.tools',
+        'metpy.vis'],
       platforms = ['Linux'],
       description = 'Collection of tools for reading, visualizing and'
         'performing calculations with weather data.',
