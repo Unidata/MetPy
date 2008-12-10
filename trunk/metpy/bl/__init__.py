661132ddcc90d876185940d72fab0cc4e1d0caf9
Sean Arms <sarms@unidata.ucar.edu>
2008-12-10 15:44:15 +0000

    first pass at reorganizing bl/ tree in metpy
    git-svn-id: http://code.forwarn.org/metpy/svn@118 150532fb-1d5b-0410-a8ab-efec50f980d4

--- trunk/metpy/bl/__init__.py
+++ trunk/metpy/bl/__init__.py
@@ -1,3 +1,15 @@
-import turb
-import sim
 import rotations
+import mos
+import fluxes
+import spectra
+import hos
+
+__version__ = '0.1'
+
+__all__ = [
+    'rotations'
+    'mos'
+    'fluxes',
+    'hos',
+    'spectra']
+
---
+++ trunk/metpy/bl/spectra.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import numpy as np
+#import scipy as S
+from matplotlib.mlab import psd as psd_welch
+from matplotlib.mlab import csd as csd_welch
+from matplotlib.mlab import cohere as coherence
+from metpy.generic import get_dims
+from scipy.integrate import cumtrapz
+
+from matplotlib import cbook
+def detrend_none(x):
+    "Return x: no detrending"
+    return x
+
+def ogive(coPxx,delf):
+    sensors,obs,blks = get_dims(coPxx)
+    coPxx=coPxx.reshape(sensors,obs,blks)
+    og = np.zeros((sensors,obs-1,blks))
+
+    ogt=cumtrapz(coPxx[:,::-1,:],dx=delf,axis=1)
+    og=ogt[:,::-1,:]
+
+    return og
