e51b3447a7dc17f89c83205697258bf3eff111a0
Sean Arms <sarms@unidata.ucar.edu>
2008-12-10 15:57:20 +0000

    combined wavelets.py and motherwavelets.py under tools/
    git-svn-id: http://code.forwarn.org/metpy/svn@119 150532fb-1d5b-0410-a8ab-efec50f980d4

--- trunk/metpy/tools/wavelets.py
+++ trunk/metpy/tools/wavelets.py
@@ -18,6 +18,114 @@ class Wavelet(object):
 
         return gws
 
+class MotherWavelet(object):
+
+    @staticmethod
+    def get_coi_coef(sampf):
+        """
+        raise error if Cone of Influence coefficient is not set in subclass wavelet
+        """
+        raise NotImplementedError('coi_coef needs to be implemented in subclass wavelet')
+
+    #add methods for computing cone of influence and mask
+    def get_coi(self):
+        """
+        Compute cone of influence
+        """
+        y1 =  self.coi_coef*np.arange(0,self.coefs.shape[1]/2)
+        y2 = -self.coi_coef*np.arange(0,self.coefs.shape[1]/2)+y1[-1]
+        coi = np.r_[y1,y2] 
+        self.coi = coi
+        return coi
+
+    def get_mask(self):
+        """
+        get mask for cone of influence.
+
+        input:
+
+            wavelet_shape : tuple containing shape of wavelet
+
+            coi_coef : cone of influence coefficient 
+
+            scales : 1d array of scales used in wavelet transform
+
+        return
+
+            mask : array of bools for use in np.ma.array('',mask=mask)
+
+        """
+        mask = np.ones(self.coefs.shape)
+        masks = self.coi_coef*self.scales
+        for s in range(0,len(self.scales)):
+            if (s != 0) and (int(np.ceil(masks[s])) < mask.shape[1]):
+                mask[s,np.ceil(int(masks[s])):-np.ceil(int(masks[s]))]=0
+        self.mask = mask.astype(bool)
+        return self.mask
+
+class SDG(MotherWavelet):
+    """
+    Class for the SDG wavelet
+
+    References
+
+    Addison, P. S., 2002: The Illustrated Wavelet Transform Handbook.  Taylor
+      and Francis Group, New York/London. 353 pp.
+
+    Collineau, S. and Y. brunet, 1993: Detection of turbulent coherent motions in a
+      forest canopy part 1: wavelet analysis.  Boundary-Layer Meteorology, 65,
+      pp 357-379.
+
+    Torrence, C., and G. P. Compo, 1998: A Practical Guide to Wavlet Analysis.
+      Bulletin of the American Meteorological Society, 79, 1, pp. 61-78.
+
+    """
+    def __init__(self,len_wavelet=None,scales=None,sampf=1,normalize=True):
+        self.sampf = sampf
+        self.scales = scales
+        self.len_wavelet = len_wavelet
+        self.normalize = normalize
+        self.coi_coef = self.get_coi_coef()
+        self.coefs = self.get_coefs()
+        self.name='second degree of a gaussian mother wavelet'    
+        self.wdu = np.pi/np.sqrt(2.); # Collineau and Brunet 1993
+
+    def get_coi_coef(self):
+        """
+        Compute the cone of influence coefficient
+        """
+        coi_coef=np.sqrt(2.)/self.sampf ;#Torrence and Compo 1998
+        self.coi_coef = coi_coef
+        return coi_coef
+
+    def get_coefs(self):
+        """
+        Calculate the coefficients for the mother wavelet SDG
+        """
+
+        """
+        Create array containing values used to evaluate the wavelet function
+        """
+        xi=np.arange(-self.len_wavelet/2.,self.len_wavelet/2.)
+
+        if self.normalize is True:
+            c=2./(np.sqrt(3.)*np.power(np.pi,0.25))
+        else:
+            c=1.
+        """
+        find coefficient at each scale
+        """
+        mw = np.empty((len(self.scales),self.len_wavelet))
+
+        for s in range(len(self.scales)):
+            xsd = -xi * xi / (self.scales[s] * self.scales[s])
+            mw[s] = c * (1. + xsd) * np.exp(xsd / 2.)
+
+        self.coefs = mw
+
+        return mw
+
+
 def cwt(x,wavelet):
     """
     cwt: compute the continuous wavelet transform of x using the mother wavelet `wavelet`.
@@ -59,30 +167,6 @@ def cxwt(x1,x2,wavelet,scales):
 
     return xwt
 
-
-
-def test():
-    import time
-    import motherwavelets as mw
-#
-#   create signal
-#
-    x=np.arange(0,100,0.1)/10*np.pi
-    y1 = np.sin(x[0:200])
-    y2=np.sin(x[200:400]*2)
-    y3=np.sin(x[400:600]*4)
-    y4=np.sin(x[600:800]*8)
-    y5=np.sin(x[800:1000]*16)
-    y=np.r_[y1,y2,y3,y4,y5]
-#
-#   get wavelet coefficients
-#
-    wt=cwt(y,mw.SDG,np.arange(1,150))
-
-
-    return wt
-
-
 """
 References
 
@@ -97,151 +181,3 @@ Torrence, C., and G. P. Compo, 1998: A Practical Guide to Wavlet Analysis.
   Bulletin of the American Meteorological Society, 79, 1, pp. 61-78.
 
 """
-#def cwt(x,wavelet,scales,calc_gws=False,gws_valid=False,sampf=1):
-#    """
-#    cwt: compute the continuous wavelet transform of x using the wavelet `wavelet`.
-#        x: signal to be transformed
-#        wavelet: wavelet from motherwavelets.py
-#        scales: 1D numpy array of scales
-#        calc_gws: compute and return the global wavelet specturm (bool)
-#        gws_valid: compute gws using only valid data (defined by cone of influence)
-#        sampf: sample frequency of x in samples per unit time
-#    """
-#    # get mother wavelets at scale a
-#    mw=wavelet.coefs(len(x),scales,sampf=sampf)
-#    # Fourier, baby!
-#    xf=fft(x)
-#    mwf=fft(mw,axis=0)
-#    # Convolve (mult. in Fourier space) and bring it back!
-#    """
-#    wt_tmp=np.abs(np.fft.ifft(mwf*xf.reshape(-1,1),axis=0).transpose())
-#    """
-#    wt_tmp=ifft(mwf*xf[np.newaxis,:],axis=0)
-#    """
-#    Compute cone of influence
-#    """
-#    y1=wavelet.coi_coef(sampf)*np.arange(0,len(x)/2)
-#    y2=-wavelet.coi_coef(sampf)*np.arange(0,len(x)/2)+y1[-1]
-#    coi = np.r_[y1,y2] 
-
-#    wt = np.fft.fftshift(wt_tmp,axes=[1])/np.sqrt(scales[:,np.newaxis])
-
-#    if np.all(np.negative(np.iscomplex(mw))):
-#        wt=wt.real
-
-#    if calc_gws is False:
-#        return coi,wt
-#    else:
-##       Compute Global Wavelet Specturm
-#        mask=get_mask(wt.shape,coi_coef,scales)
-
-#        if gws_valid is True:
-#            gws = get_gws(wt,mask=mask)
-#        else:
-#            gws = get_gws(wt)
-
-#        return coi,gws,wt
-#def get_mask(wavelet_shape,coi_coef,scales):
-#    """
-#    get mask for cone of influence.
-
-#    input:
-
-#        wavelet_shape : tuple containing shape of wavelet
-
-#        coi_coef : cone of influence coefficient 
-
-#        scales : 1d array of scales used in wavelet transform
-
-#    return
-
-#        mask : array of bools for use in np.ma.array('',mask=mask)
-
-#    """
-#    mask = np.ones(wavelet_shape)
-#    masks = coi_coef*scales
-#    for s in range(0,len(scales)):
-#        if (s != 0) and (int(np.ceil(masks[s])) < mask.shape[1]):
-#            mask[s,np.ceil(int(masks[s])):-np.ceil(int(masks[s]))]=0
-
-#    return mask.astype(bool)
-
-
-#def SDG(len_wavelet,scales,normalize=True,return_wdu=False,sampf=1):
-#    """
-#    Compute the Second Derivative of a Gaussian (SDG) wavelet of length
-#    'len_wavelet' at scales 'scales' (numpy array) in time domain.
-
-#    for normalize = True (give wavelet unit energy)
-#        W(a)=2/(sqrt(3)*PI^(0.25))*(1-x^2)*exp(-x^2/2) (Addison 2002)
-#    for normalize = False
-#        W(a)=(1-x^2)*exp(-x^2/2) (Addison 2002)
-
-#    len_wavelet: length of wavelet
-
-#    return_wdu : return the wavelet durration unit (relates scale to period)
-
-#    The returned coefficients are centered (x=0) at len_w/2
-#    """
-#    coi_coef=np.sqrt(2.)/sampf ;#Torrence and Compo 1998
-#    wdu = np.pi/np.sqrt(2.); # Collineau and Brunet 1993
-##
-##   Create array containing values used to evaluate the wavelet function
-##
-#    xi=np.arange(-len_wavelet/2.,len_wavelet/2.)
-##
-##   Compute wavelet function
-##
-#    if normalize is True:
-#        c=2./(np.sqrt(3.)*np.power(np.pi,0.25))
-#    else:
-#        c=1.
-
-#    for s in np.arange(0,len(scales)):
-#        xsd=xi/scales[s]
-#        if s != 0:
-#            mw_tmp = c*(1.-xsd*xsd)*np.exp(-xsd*xsd/2.)
-#            mw=np.c_[mw,mw_tmp]
-#        else:
-#            mw = c*(1.-xsd*xsd)*np.exp(-xsd*xsd/2.)
-#    if return_wdu:
-#        return coi_coef,wdu,mw
-#    else:
-#        return coi_coef,mw
-
-#def cwt_time(x,wavelet,max_scale):
-#    """
-#    Compute the wavelet transform (in the time domain using direct convolution)
-#    of 'x' using the 'wavelet' basis set from scales 1 to 'max_scale'.
-
-#    x - data to which the wavelet transform is applied
-
-#    wavelet - wavelet function (for example, SDG)
-
-#    max_scale - maximum scale used to compute the wavelet transform
-#    """
-#    wt=[]
-##
-##   loop over scales from 1 to max_scale
-##
-#    for a in range(1,max_scale+1):
-##        if a in range(0,max_scale,100):
-##            print '    working on ' +`a` + "'s scale"
-##
-##       length of wavelet function (based on scale to mitigate truncation of
-##         wavelet function - 10*a seems to work well for the SDG wavelet, may need
-##         to be tweaked for others.
-##
-#        len_w=len(x)+10*a
-##
-##       indicies to pull out array of len(x) centered on the full wavelet vector
-##
-#        indexa=(len_w-len(x))/2
-#        indexb=(len_w+len(x))/2
-##
-##       compute wavelet coefficients at scale a
-##
-#        wt.append((1./np.sqrt(a)*np.convolve(x,np.conjugate(wavelet(len_w,a)),mode='same'))[indexa:indexb])
-
-#    return np.array(wt)
-
