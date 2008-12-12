d7b7f157fd5758c1de22810d871642768f4eac68
Ryan May <rmay31@gmail.com>
2008-12-12 16:34:26 +0000

    Add mesonet readers to top level namespace.
    git-svn-id: http://code.forwarn.org/metpy/svn@150 150532fb-1d5b-0410-a8ab-efec50f980d4

--- trunk/metpy/__init__.py
+++ trunk/metpy/__init__.py
@@ -3,7 +3,10 @@ import readers
 import vis
 import tools
 import constants
+
+#What do we want to pull into the top-level namespace
 from calc import *
+from readers.mesonet import *
 
 import version
 __version__ = version.get_version()
