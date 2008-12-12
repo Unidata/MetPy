6bd9433f2cc43df12ecbd52730aa09cdc93312da
Sean Arms <sarms@unidata.ucar.edu>
2008-12-12 16:20:35 +0000

    changed __init__ for tools and bl to use extend method for __all__ following lib in numpy
    git-svn-id: http://code.forwarn.org/metpy/svn@144 150532fb-1d5b-0410-a8ab-efec50f980d4

--- trunk/metpy/bl/__init__.py
+++ trunk/metpy/bl/__init__.py
@@ -4,10 +4,10 @@ from fluxes import *
 from spectra import *
 from hos import *
 
-__all__ = [
-    'rotations'
-    'mos'
-    'fluxes',
-    'hos',
-    'spectra']
+__all__ = []
+__all__.extend(rotations.__all__)
+__all__.extend(mos.__all__)
+__all__.extend(fluxes.__all__)
+__all__.extend(spectra.__all__)
+__all__.extend(hos.__all__)
 
--- trunk/metpy/tools/__init__.py
+++ trunk/metpy/tools/__init__.py
@@ -2,3 +2,8 @@ from eigana import *
 from stats import *
 from wavelets import *
 
+__all__ = []
+__all__.extend(eigana.__all__)
+__all__.extend(stats.__all__)
+__all__.extend(wavelets.__all__)
+
