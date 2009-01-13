Fix bug in non-PyEphem calculation of cosine of sun zenith angle.
git-svn-id: http://code.forwarn.org/metpy/svn@211 150532fb-1d5b-0410-a8ab-efec50f980d4

trunk/metpy/tools/solar.py
@@ -65,7 +65,7 @@ except ImportError:
         return earth_max_declination * np.cos(solar_longitude) * degree
 
     def _get_cos_zenith(lat, lon, dt):
-        delta = solar_declination_angle(dt)
+        delta = solar_declination_angle(dt[0])
         hour = np.array([d.hour + d.minute/60. + d.second/3600. for d in dt])
         hour_angle = 15 * hour * degree
 