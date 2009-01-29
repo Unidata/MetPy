2937e899daf612376a0e617a095fa733f2114c02
Ryan May <rmay31@gmail.com>
2009-01-29 20:59:18 +0000

    Forgot namespace on Normalize.
    git-svn-id: http://code.forwarn.org/metpy/svn@257 150532fb-1d5b-0410-a8ab-efec50f980d4

--- trunk/metpy/vis/ctables.py
+++ trunk/metpy/vis/ctables.py
@@ -51,8 +51,8 @@ _NWSRef_data = {'blue': [(0.0, 0.92549019607843142, 0.92549019607843142),
     (0.85714286, 1.0, 1.0),
     (0.92857143, 0.59999999999999998, 0.59999999999999998),
     (1.0, 0.0, 0.0)]}
-NWSRefPrecip = Normalize(5,75)
-NWSRefClearAir = Normalize(-28, 28)
+NWSRefPrecip = colors.Normalize(5,75)
+NWSRefClearAir = colors.Normalize(-28, 28)
 
 _NWSVel_data = {'blue': [(0.0, 0.62352941176470589, 0.62352941176470589),
  (0.071428571428571425, 0.0, 0.0),
