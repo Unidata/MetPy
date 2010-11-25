ab6c1dce403f7403c3ceb588536e8dac44208c63
Patrick Marsh <patrickmarshwx@gmail.com>
2010-11-24 19:11:44 -0600

    Added routines that will allow additional cython files. Switched from looking for SVN to GIT for versioning. Added routine to remove .html or .pyc files after running setup.py. Added cleanup routine to remove build directory when doing an install.

--- setup.py
+++ setup.py
@@ -1,12 +1,52 @@
+from __future__ import print_function, division
+import distutils.sysconfig
 from distutils.core import setup
-import os.path, sys
+from distutils.extension import Extension
+from distutils.command.install_data import install_data
+import numpy as np
+import os, sys
 
-build_gauss = False
+build_gauss = True
+
+# Scan directory for extension files, converting
+# them to extension names in dotted notation
+def scandir(dir, files=[]):
+    for file in os.listdir(dir):
+        path = os.path.join(dir, file)
+        if os.path.isfile(path) and path.endswith(".pyx"):
+            files.append(path.replace(os.path.sep, ".")[:-4])
+        elif os.path.isdir(path):
+            scandir(path, files)
+    return files
+
+# Scan directory for extension files, removing compiled
+# or html files
+def cleanup(dir, fext):
+    for file in os.listdir(dir):
+        path = os.path.join(dir, file)
+        if os.path.isfile(path) and path.endswith(fext):
+            os.remove(path)
+        elif os.path.isdir(path):
+            cleanup(path, fext)
+
+
+# Generate an Extension object from its dotted name
+def makeExtension(extName):
+    extPath = extName.replace(".", os.path.sep)+".pyx"
+    return Extension(
+        extName,
+        [extPath],
+        include_dirs = [np.get_include(), "."],   # the '.' is CRUCIAL!!
+        extra_compile_args = ["-O3", "-Wall"],
+        extra_link_args = [],
+        libraries = [],
+        )
+
 
 setup_path = os.path.split(os.path.abspath(__file__))[0]
 sys.path.append(os.path.join(setup_path, 'metpy'))
 import version
-version.write_svn_version()
+version.write_git_version()
 ver = version.get_version()
 sys.path.pop()
 
@@ -32,21 +72,27 @@ else:
     include_dirs = None
     build_ext = None
 
-setup(name = 'MetPy',
-      version = ver,
-      packages = ['metpy', 'metpy.bl', 'metpy.readers', 'metpy.tools',
-        'metpy.vis'],
-      ext_modules = ext_modules,
-      include_dirs = include_dirs,
-      cmdclass = {'build_ext':build_ext},
-      platforms = ['Linux'],
-      description = 'Collection of tools for reading, visualizing and'
-        'performing calculations with weather data.',
-      url = 'http://code.forwarn.org/metpy',
-      )
+setup(
+    name            = 'MetPy',
+    version         = ver,
+    packages        = ['metpy', 'metpy.bl', 'metpy.readers', 'metpy.tools',
+                       'metpy.vis'],
+    ext_modules     = ext_modules,
+    include_dirs    = include_dirs,
+    cmdclass        = {'build_ext':build_ext},
+    platforms       = ['Linux'],
+    description     = 'Collection of tools for reading, visualizing and'
+                      'performing calculations with weather data.',
+    url             = 'http://code.forwarn.org/metpy',)
 
 if not version.release:
-  # Remove __svn_version__ so that if we run from local, an outdated version
+  # Remove __git_version__ so that if we run from local, an outdated version
   # won't be found
-  os.remove(version._svn_file_path)
-  os.remove(version._svn_file_path + 'c') #The .pyc file
+  os.remove(version._git_file_path)
+  os.remove(version._git_file_path + 'c') #The .pyc file
+
+  cleanup('.', '.html')
+  cleanup('.', '.pyc')
+  if sys.argv[1] in ['install']:
+      import shutil
+      shutil.rmtree('build')
