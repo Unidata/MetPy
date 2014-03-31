81d561fa6950a1019acc9f71dcb1c8b35dd89e6e
Ryan May <rmay31@gmail.com>
2014-03-31 16:03:11 -0600

    Add new plot elements to Skew-T example.

--- examples/skewt.py
+++ examples/skewt.py
@@ -2,11 +2,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from metpy.calc import get_wind_components
 from metpy.plots import SkewT
 
 # Parse the data
-p, h, T, Td = np.loadtxt('testdata/sounding_data.txt', usecols=range(0, 4),
-        unpack=True)
+p, T, Td, direc, spd = np.loadtxt('testdata/sounding_data.txt',
+        usecols=(0, 2, 3, 6, 7), unpack=True)
+u,v = get_wind_components(spd, direc)
 
 # Create a new figure. The dimensions here give a good aspect ratio
 fig = plt.figure(figsize=(6.5875, 6.2125))
@@ -16,6 +18,7 @@ skew = SkewT(fig)
 # log scaling in Y, as dictated by the typical meteorological plot
 skew.plot(p, T, 'r')
 skew.plot(p, Td, 'g')
+skew.plot_barbs(p, u, v)
 
 # Example of coloring area between profiles
 skew.ax.fill_betweenx(p, T, 0, where=T>=0, facecolor='red', alpha=0.4)
@@ -24,6 +27,10 @@ skew.ax.fill_betweenx(p, T, 0, where=T>=0, facecolor='red', alpha=0.4)
 # isotherm
 l = skew.ax.axvline(0, color='c', linestyle='--', linewidth=2)
 
+# Add the relevant special lines
 skew.plot_dry_adiabats()
+skew.plot_moist_adiabats()
 skew.plot_mixing_lines()
+
+# Show the plot
 plt.show()
