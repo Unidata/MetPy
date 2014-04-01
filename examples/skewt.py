41b384428fce71412fb1bfb1e9e76e7619f78a0a
Ryan May <rmay31@gmail.com>
2014-03-31 23:16:44 -0600

    Add parcel profile to Skew-T example.

--- examples/skewt.py
+++ examples/skewt.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.constants import C2K, K2C
 
-from metpy.calc import get_wind_components
+from metpy.calc import get_wind_components, lcl, dry_lapse, parcel_profile
 from metpy.plots import SkewT
 
 # Parse the data
@@ -20,8 +21,12 @@ skew.plot(p, T, 'r')
 skew.plot(p, Td, 'g')
 skew.plot_barbs(p, u, v)
 
-# Example of coloring area between profiles
-skew.ax.fill_betweenx(p, T, 0, where=T>=0, facecolor='red', alpha=0.4)
+l = lcl(p[0], T[0], Td[0])
+skew.plot(l, K2C(dry_lapse(l, C2K(T[0]), p[0])), 'ko',
+        markerfacecolor='black')
+
+prof = parcel_profile(p, T[0], Td[0])
+skew.plot(p, prof, 'k', linewidth=2)
 
 # An example of a slanted line at constant T -- in this case the 0
 # isotherm
