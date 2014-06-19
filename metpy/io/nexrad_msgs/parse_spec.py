43b426d9bafb32aa0a113c5cfe3019dac63c798b
Ryan May <rmay31@gmail.com>
2014-06-19 15:12:15 -0600

    Refactor generation script.
    This makes it easier to handle multiple files.

--- metpy/io/nexrad_msgs/parse_spec.py
+++ metpy/io/nexrad_msgs/parse_spec.py
@@ -1,72 +1,90 @@
 #!/usr/bin/env python
-import os.path
-import sys
 
-with open(sys.argv[1], 'r') as infile:
-    info = []
-    for lineno,line in enumerate(infile):
-        parts = line.split('  ')
-        try:
-            if len(parts) == 8:
-                parts = parts[:6] + [parts[6] + parts[7]]
+processors = {}
+def register_processor(num):
+    def inner(func):
+        processors[num] = func
+        return func
+    return inner
 
-            var_name,desc,typ,units,rng,prec,byte_range = parts
-            start,end = map(int, byte_range.split('-'))
-            size = end - start + 1
-            assert size >= 4
-            if typ == 'Real*4':
-                typ = 'f'
-                assert size == 4, 'Got size %d' % size
-            elif typ == 'Integer*4':
-                typ = 'L'
-                assert size == 4, 'Got size %d' % size
-            elif typ == 'SInteger*4':
-                typ = 'l'
-                assert size == 4, 'Got size %d' % size
-            elif typ.startswith('String'):
-                typ = '%ds' % size
-            elif not typ or typ == 'N/A':
-                if var_name != 'SPARE':
-                    print('WARNING: %s has type %s. Setting as SPARE' % (var_name, typ))
-                    var_name = 'SPARE'
-                typ = '%dx' % size
-            elif typ == 'See Note (5)':
-                typ = '%ds' % size
-                assert size == 1172
-            else:
-                raise ValueError('No type match! (%s)' % typ)
-            full_desc = desc.strip()
-            if units and units != 'N/A':
-                full_desc += ' (' + units + ')'
+@register_processor(18)
+def process_msg18(fname):
+    with open(fname, 'r') as infile:
+        info = []
+        for lineno,line in enumerate(infile):
+            parts = line.split('  ')
+            try:
+                if len(parts) == 8:
+                    parts = parts[:6] + [parts[6] + parts[7]]
 
-            name = var_name.strip()
-            for char in '().':
-                name = name.replace(char, '_')
-            if name.endswith('_'):
-                name = name[:-1]
-            info.append((name, full_desc, typ))
+                var_name,desc,typ,units,rng,prec,byte_range = parts
+                start,end = map(int, byte_range.split('-'))
+                size = end - start + 1
+                assert size >= 4
+                if typ == 'Real*4':
+                    typ = 'f'
+                    assert size == 4, 'Got size %d' % size
+                elif typ == 'Integer*4':
+                    typ = 'L'
+                    assert size == 4, 'Got size %d' % size
+                elif typ == 'SInteger*4':
+                    typ = 'l'
+                    assert size == 4, 'Got size %d' % size
+                elif typ.startswith('String'):
+                    typ = '%ds' % size
+                elif not typ or typ == 'N/A':
+                    if var_name != 'SPARE':
+                        print('WARNING: %s has type %s. Setting as SPARE' % (var_name, typ))
+                        var_name = 'SPARE'
+                    typ = '%dx' % size
+                elif typ == 'See Note (5)':
+                    typ = '%ds' % size
+                    assert size == 1172
+                else:
+                    raise ValueError('No type match! (%s)' % typ)
+                full_desc = desc.strip()
+                if units and units != 'N/A':
+                    full_desc += ' (' + units + ')'
 
-            if ' ' in var_name:
-                print('WARNING: space in %s' % var_name)
-            if not desc:
-                print('WARNING: null description for %s' % var_name)
-        except ValueError:
-            print('%d > %s' % (lineno + 1, ':'.join(parts)))
-            raise
+                name = var_name.strip()
+                for char in '().':
+                    name = name.replace(char, '_')
+                if name.endswith('_'):
+                    name = name[:-1]
+                info.append((name, full_desc, typ))
 
-with open(os.path.splitext(sys.argv[1])[0] + '.py', 'w') as outfile:
-    # File header
-    outfile.write('# Generated file -- do not modify\n')
+                if ' ' in var_name:
+                    print('WARNING: space in %s' % var_name)
+                if not desc:
+                    print('WARNING: null description for %s' % var_name)
+            except ValueError:
+                print('%d > %s' % (lineno + 1, ':'.join(parts)))
+                raise
+        return info
 
-    # Variable descriptions
-    outfile.write('descriptions = {')
-    outdata = '\n    '.join('"{0}" : "{1}",'.format(*i) for i in info if i[0] != 'SPARE')
-    outfile.write(outdata)
-    outfile.write('}\n\n')
+def write_file(fname, info):
+    with open(fname, 'w') as outfile:
+        # File header
+        outfile.write('# Generated file -- do not modify\n')
 
-    # Now the struct format
-    outfile.write('fields = [')
-    outdata = '\n    '.join('({0}, "{1}"),'.format('"' + name + '"' if name != 'SPARE' else None, fmt)
-            for name,__,fmt in info)
-    outfile.write(outdata)
-    outfile.write(']\n')
+        # Variable descriptions
+        outfile.write('descriptions = {')
+        outdata = '\n    '.join('"{0}" : "{1}",'.format(*i) for i in info if i[0] != 'SPARE')
+        outfile.write(outdata)
+        outfile.write('}\n\n')
+
+        # Now the struct format
+        outfile.write('fields = [')
+        outdata = '\n    '.join('({0}, "{1}"),'.format('"' + name + '"' if name != 'SPARE' else None, fmt)
+                for name,__,fmt in info)
+        outfile.write(outdata)
+        outfile.write(']\n')
+
+if __name__ == '__main__':
+    import os.path
+
+    for num in [18]:
+        fname = 'msg%d.spec' % num
+        info = processors[num](fname)
+        fname = os.path.splitext(fname)[0] + '.py'
+        write_file(fname, info)
