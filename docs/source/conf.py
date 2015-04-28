dad838dc44daff4e37e179df4fcd8204794d883e
Ryan May <rmay31@gmail.com>
2015-04-28 14:29:56 -0600

    Fix displaying equations on readthedocs.
    Was requesting mathjax via http on https site. See
    sphinx-doc/sphinx#1459

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinxcontrib.napoleon',
 ]
 
+mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 #autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
