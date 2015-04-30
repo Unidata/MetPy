6c9211e3c890ce6e8ac59a5b39578a4845912ea3
Ryan May <rmay31@gmail.com>
2015-04-30 14:10:04 -0600

    Add more test files for level 2.

--- metpy/io/tests/test_nexrad.py
+++ metpy/io/tests/test_nexrad.py
@@ -7,7 +7,7 @@ curdir, f = os.path.split(__file__)
 datadir = os.path.join(curdir, '../../../examples/testdata')
 
 
-def test_generator():
+def test_level3_generator():
     for fname in glob.glob(os.path.join(datadir, 'nids', 'KOUN*')):
         yield read_level3_file, fname
 
@@ -15,15 +15,24 @@ def test_generator():
 def read_level3_file(fname):
     Level3File(fname)
 
+# 1999 file tests old message 1
+# KFTG tests bzip compression and newer format for a part of message 31
+level2_files = ['KTLX20130520_201643_V06.gz', 'KTLX19990503_235621.gz',
+                'Level2_KFTG_20150430_1419.ar2v']
+
+
+def test_level2_generator():
+    for fname in level2_files:
+        yield read_level2_file, os.path.join(datadir, fname)
+
+
+def read_level2_file(fname):
+    Level2File(fname)
+
 
 class TestLevel3(TestCase):
     def test_basic(self):
         Level3File(os.path.join(datadir, 'nids/Level3_FFC_N0Q_20140407_1805.nids'))
-
-
-class TestLevel2(TestCase):
-    def test_basic(self):
-        Level2File(os.path.join(datadir, 'KTLX20130520_201643_V06.gz'))
 
 
 class TestPrecipMode(TestCase):
