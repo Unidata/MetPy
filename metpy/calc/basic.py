Add/fix up helpers for units.
- Add helper for atleast_[1,2]d and use those
- Change calling of concatenate to take sequence so we can specify axis.
  Update all callers of this.

metpy/calc/basic.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.ma import masked_array
 from ..package_tools import Exporter
-from ..units import units
+from ..units import atleast_1d, units
 
 exporter = Exporter(globals())
 
@@ -28,7 +28,7 @@ def get_speed_dir(u, v):
     '''
 
     speed = np.sqrt(u * u + v * v)
-    wdir = units.Quantity(np.atleast_1d(90. * units.deg - np.arctan2(v, u)), units.deg)
+    wdir = atleast_1d(90. * units.deg - np.arctan2(v, u))
     wdir[wdir < 0] += 360. * units.deg
     return speed, wdir.reshape(speed.shape)
 

metpy/calc/thermo.py
@@ -3,7 +3,7 @@ import scipy.integrate as si
 from numpy.ma import log, exp
 from ..package_tools import Exporter
 from ..constants import epsilon, kappa, P0, Rd, Lv, Cp_d
-from ..units import units, concatenate
+from ..units import atleast_1d, concatenate, units
 
 exporter = Exporter(globals())
 
@@ -133,7 +133,7 @@ def moist_lapse(pressure, temperature):
         rs = mixing_ratio(saturation_vapor_pressure(t), p)
         return (1. / p) * ((Rd * t + Lv * rs) /
                            (Cp_d + (Lv * Lv * rs * epsilon / (Rd * t * t))))
-    return units.Quantity(si.odeint(dt, np.atleast_1d(temperature).squeeze(),
+    return units.Quantity(si.odeint(dt, atleast_1d(temperature).squeeze(),
                                     pressure.squeeze()).T.squeeze(), temperature.units)
 
 
@@ -227,15 +227,15 @@ def parcel_profile(pressure, temperature, dewpt):
     l = lcl(pressure[0], temperature, dewpt).to(pressure.units)
 
     # Find the dry adiabatic profile, *including* the LCL
-    press_lower = concatenate(pressure[pressure > l], l)
+    press_lower = concatenate((pressure[pressure > l], l))
     t1 = dry_lapse(press_lower, temperature)
 
     # Find moist pseudo-adiabatic profile starting at the LCL
-    press_upper = concatenate(l, pressure[pressure < l])
+    press_upper = concatenate((l, pressure[pressure < l]))
     t2 = moist_lapse(press_upper, t1[-1]).to(t1.units)
 
     # Return LCL *without* the LCL point
-    return concatenate(t1[:-1], t2[1:])
+    return concatenate((t1[:-1], t2[1:]))
 
 
 @exporter.export

metpy/units.py
@@ -16,7 +16,7 @@ import numpy as np
 units = pint.UnitRegistry(autoconvert_offset_to_baseunit=True)
 
 
-def concatenate(*args):
+def concatenate(arrs, axis=0):
     r'''Concatenate multiple values into a new unitized object.
 
     This is essentially a unit-aware version of `numpy.concatenate`. All items
@@ -26,7 +26,7 @@ def concatenate(*args):
 
     Parameters
     ----------
-    args : arbitrary positional argument
+    arrs : Sequence of arrays
          The items to be joined together
 
     Returns
@@ -34,17 +34,71 @@ def concatenate(*args):
     `pint.Quantity`
         New container with the value passed in and units corresponding to the first item.
     '''
-    for a in args:
+
+    dest = 'dimensionless'
+    for a in arrs:
         if hasattr(a, 'units'):
             dest = a.units
             break
 
     data = []
-    for a in args:
+    for a in arrs:
         if hasattr(a, 'to'):
             a = a.to(dest).magnitude
         data.append(np.atleast_1d(a))
 
-    return units.Quantity(np.concatenate(data), dest)
+    return units.Quantity(np.concatenate(data, axis=axis), dest)
+
+
+def atleast_1d(*arrs):
+    r'''Convert inputs to arrays with at least one dimension
+
+    Scalars are converted to 1-dimensional arrays, whilst other
+    higher-dimensional inputs are preserved. This is a thin wrapper
+    around `numpy.atleast_1d` to preserve units.
+
+    Parameters
+    ----------
+    arrs : arbitrary positional arguments
+        Input arrays to be converted if necessary
+
+    Returns
+    -------
+    `pint.Quantity`
+        A single quantity or a list of quantities, matching the number of inputs.
+    '''
+
+    mags = [a.magnitude for a in arrs]
+    orig_units = [a.units for a in arrs]
+    ret = np.atleast_1d(*mags)
+    if len(mags) == 1:
+        return units.Quantity(ret, orig_units[0])
+    return [units.Quantity(m, u) for m, u in zip(ret, orig_units)]
+
+
+def atleast_2d(*arrs):
+    r'''Convert inputs to arrays with at least two dimensions
+
+    Scalars and 1-dimensional arrays are converted to 2-dimensional arrays,
+    whilst other higher-dimensional inputs are preserved. This is a thin wrapper
+    around `numpy.atleast_2d` to preserve units.
+
+    Parameters
+    ----------
+    arrs : arbitrary positional arguments
+        Input arrays to be converted if necessary
+
+    Returns
+    -------
+    `pint.Quantity`
+        A single quantity or a list of quantities, matching the number of inputs.
+    '''
+
+    mags = [a.magnitude for a in arrs]
+    orig_units = [a.units for a in arrs]
+    ret = np.atleast_2d(*mags)
+    if len(mags) == 1:
+        return units.Quantity(ret, orig_units[0])
+    return [units.Quantity(m, u) for m, u in zip(ret, orig_units)]
 
 del pint