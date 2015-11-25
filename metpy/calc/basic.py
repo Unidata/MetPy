Change relative humidity units for heat_index. (Fixes #84)
Conform to our standard of a ratio in the range [0, 1]. Percentages can
be passed by making use of the `percent` unit we define.

metpy/calc/basic.py
@@ -174,7 +174,8 @@ def heat_index(temperature, rh, mask_undefined=True):
     temp : array_like
         Air temperature
     rh : array_like
-        The relative humidity expressed as a percentage in the range [0, 100].
+        The relative humidity expressed as a unitless ratio in the range [0, 1].
+        Can also pass a percentage if proper units are attached.
 
     Returns : array_like
         The corresponding Heat Index value(s)
@@ -205,16 +206,16 @@ def heat_index(temperature, rh, mask_undefined=True):
     rh2 = rh ** 2
     delta2 = delta ** 2
 
-    # Calculate the Heat Index
+    # Calculate the Heat Index -- constants converted for RH in [0, 1]
     hi = (-42.379 * units.degF + 2.04901523 * delta +
-          10.14333127 * units.delta_degF * rh - 0.22475541 * delta * rh -
-          6.83783e-3 / units.delta_degF * delta2 - 5.481717e-2 * units.delta_degF * rh2 +
-          1.22874e-3 / units.delta_degF * delta2 * rh + 8.5282e-4 * delta * rh2 -
-          1.99e-6 / units.delta_degF * delta2 * rh2)
+          1014.333127 * units.delta_degF * rh - 22.475541 * delta * rh -
+          6.83783e-3 / units.delta_degF * delta2 - 5.481717e2 * units.delta_degF * rh2 +
+          1.22874e-1 / units.delta_degF * delta2 * rh + 8.5282 * delta * rh2 -
+          1.99e-2 / units.delta_degF * delta2 * rh2)
 
     # See if we need to mask any undefined values
     if mask_undefined:
-        mask = np.array((temperature < 80. * units.degF) | (rh < 40))
+        mask = np.array((temperature < 80. * units.degF) | (rh < 40 * units.percent))
         if mask.any():
             hi = units.Quantity(masked_array(hi, mask=mask), hi.units)
 

metpy/calc/tests/test_basic.py
@@ -109,20 +109,20 @@ class TestHeatIndex(object):
     def test_basic(self):
         'Test the basic heat index calculation.'
         temp = np.array([80, 88, 92, 110]) * units.degF
-        rh = np.array([40, 100, 70, 40])
+        rh = np.array([40, 100, 70, 40]) * units.percent
 
         hi = heat_index(temp, rh)
         values = np.array([80, 121, 112, 136]) * units.degF
         assert_array_almost_equal(hi, values, 0)
 
     def test_scalar(self):
-        hi = heat_index(96 * units.degF, 65)
+        hi = heat_index(96 * units.degF, 65 * units.percent)
         assert_almost_equal(hi, 121 * units.degF, 0)
 
     def test_invalid(self):
         'Test for values that should be masked.'
         temp = np.array([80, 88, 92, 79, 30, 81]) * units.degF
-        rh = np.array([40, 39, 2, 70, 50, 39])
+        rh = np.array([40, 39, 2, 70, 50, 39]) * units.percent
 
         hi = heat_index(temp, rh)
         mask = np.array([False, True, True, True, True, True])
@@ -131,7 +131,7 @@ class TestHeatIndex(object):
     def test_undefined_flag(self):
         'Tests whether masking values can be disabled.'
         temp = units.Quantity(np.ma.array([80, 88, 92, 79, 30, 81]), units.degF)
-        rh = np.ma.array([40, 39, 2, 70, 50, 39])
+        rh = np.ma.array([40, 39, 2, 70, 50, 39]) * units.percent
 
         hi = heat_index(temp, rh, mask_undefined=False)
         mask = np.array([False] * 6)
@@ -140,10 +140,16 @@ class TestHeatIndex(object):
     def test_units(self):
         'Test units coming out of heat index'
         temp = units.Quantity([35., 20.], units.degC)
-        rh = 70.
+        rh = 70 * units.percent
         hi = heat_index(temp, rh)
         assert_almost_equal(hi.to('degC'), units.Quantity([50.3405, np.nan], units.degC), 4)
 
+    def test_ratio(self):
+        'Test giving humidity as number [0, 1]'
+        temp = units.Quantity([35., 20.], units.degC)
+        rh = 0.7
+        hi = heat_index(temp, rh)
+        assert_almost_equal(hi.to('degC'), units.Quantity([50.3405, np.nan], units.degC), 4)
 
 # class TestIrrad(object):
 #    def test_basic(self):