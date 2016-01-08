254c0220468068bad4a82fb55a7398cd1e044bb7
Ryan May <rmay31@gmail.com>
2016-01-07 17:37:37 -0700

    Add station plots.
    Adds a class to do station plotting, as well as a class that can be used
    to create canned layouts that pulls data out of a dict-like object.

--- metpy/plots/__init__.py
+++ metpy/plots/__init__.py
@@ -2,8 +2,11 @@
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from . import _mpl  # Trigger matplotlib wrappers
+# Trigger matplotlib wrappers
+from . import _mpl  # noqa
 
 from .skewt import *  # noqa
+from .station_plot import *  # noqa
 __all__ = []
 __all__.extend(skewt.__all__)  # pylint: disable=undefined-variable
+__all__.extend(station_plot.__all__)  # pylint: disable=undefined-variable
