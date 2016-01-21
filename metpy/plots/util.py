b84101ef5f481d44b91114491a7552f33e6ea0d3
Alex Haberlie <ahaberlie@niu.edu>
2016-01-21 16:11:24 -0600

    Added 1d nearest neighbor helper function
    Allow resampling based on given data and centers
    
    Added an example to Simple Sounding notebook

--- metpy/plots/util.py
+++ metpy/plots/util.py
@@ -7,6 +7,27 @@ from matplotlib.collections import LineCollection
 
 from ..units import concatenate
 
+def NN_resample_1d(a,centers):
+    
+    """Helper function that returns one-dimensional nearest-neighbor indexes based on user-specified centers.
+    ----------
+    a : array-like
+        1-dimensional array of numeric values from which to extract indexes of nearest-neighbors
+    centers : array-like
+        1-dimensional array of numeric values representing a subset of values to approximate
+    Returns
+    -------
+        An array of indexes representing values closest to given array values
+    
+    """
+        
+    ix = []
+    for center in centers:
+        index = (np.abs(a-center)).argmin()
+        if index not in ix:
+            ix.append(index)
+            
+    return ix
 
 # Not part of public API
 def colored_line(x, y, c, **kwargs):
