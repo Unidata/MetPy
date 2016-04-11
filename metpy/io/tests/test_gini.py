30e1a4c9d2d411998790dff1ce0b6d6d70759979
Jonathan Helmus <jjhelmus@gmail.com>
2016-04-11 14:46:56 -0500

    TST: Add tests for AK-REGIONAL Gini files
    Sample AK-REGIONAL Gini file included in testdata directory

--- metpy/io/tests/test_gini.py
+++ metpy/io/tests/test_gini.py
@@ -48,6 +48,42 @@ def test_gini_basic():
     assert f.data.shape, (pdb.num_records == pdb.record_len)
 
 
+def test_gini_ak_regional():
+    'Test reading of AK Regional Gini file'
+    f = GiniFile(get_test_data('AK-REGIONAL_8km_3.9_20160408_1445.gini'))
+    pdb = f.prod_desc
+    assert pdb.source == 1
+    assert pdb.creating_entity == 'GOES-15'
+    assert pdb.sector_id == 'Alaska Regional'
+    assert pdb.channel == 'IR (3.9 micron)'
+    assert pdb.num_records == 408
+    assert pdb.record_len == 576
+    assert pdb.datetime, datetime(2016, 4, 8, 14, 45, 20 == 0)
+    assert pdb.projection == GiniProjection.polar_stereographic
+    assert pdb.nx == 576
+    assert pdb.ny == 408
+    assert_almost_equal(pdb.la1, 42.0846, 4)
+    assert_almost_equal(pdb.lo1, -175.641, 4)
+
+    proj = f.proj_info
+    assert proj.reserved == 0
+    assert_almost_equal(proj.lov, 210.0, 1)
+    assert_almost_equal(proj.dx, 7.9375, 4)
+    assert_almost_equal(proj.dy, 7.9375, 4)
+    assert proj.proj_center == 0
+
+    pdb2 = f.prod_desc2
+    assert pdb2.scanning_mode == [False, False, False]
+    assert_almost_equal(pdb2.lat_in, 0.0, 4)
+    assert pdb2.resolution == 8
+    assert pdb2.compression == 0
+    assert pdb2.version == 1
+    assert pdb2.pdb_size == 512
+    assert pdb2.nav_cal == 0
+
+    assert f.data.shape, (pdb.num_records == pdb.record_len)
+
+
 def test_gini_bad_size():
     'Test reading a GINI file that reports a bad header size'
     f = GiniFile(get_test_data('NHEM-MULTICOMP_1km_IR_20151208_2100.gini'))
@@ -64,6 +100,19 @@ def test_gini_dataset():
     assert 'WV' in ds.variables
     assert hasattr(ds.variables['WV'], 'grid_mapping')
     assert ds.variables['WV'].grid_mapping in ds.variables
+    assert_almost_equal(ds.variables['lon'][0, 0], f.prod_desc.lo1, 4)
+    assert_almost_equal(ds.variables['lat'][0, 0], f.prod_desc.la1, 4)
+
+
+def test_gini_ak_regional_dataset():
+    'Test the dataset interface for GINI of a AK REGIONAL file'
+    f = GiniFile(get_test_data('AK-REGIONAL_8km_3.9_20160408_1445.gini'))
+    ds = f.to_dataset()
+    assert 'x' in ds.variables
+    assert 'y' in ds.variables
+    assert 'IR' in ds.variables
+    assert hasattr(ds.variables['IR'], 'grid_mapping')
+    assert ds.variables['IR'].grid_mapping in ds.variables
     assert_almost_equal(ds.variables['lon'][0, 0], f.prod_desc.lo1, 4)
     assert_almost_equal(ds.variables['lat'][0, 0], f.prod_desc.la1, 4)
 
