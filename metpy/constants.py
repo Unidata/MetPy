a86ec91193a03711a8936814fcab8f051345a5e6
kpozsonyi <knpozson@millersville.edu>
2016-06-30 13:39:46 -0600

    pep8

--- metpy/constants.py
+++ metpy/constants.py
@@ -80,9 +80,9 @@ exporter = Exporter(globals())
 with exporter:
     # Earth
     earth_gravity = g = units.Quantity(1.0, units.gravity).to('m / s^2')
-    #Taken from GEMPAK constants
+    # Taken from GEMPAK constants
     Re = earth_avg_radius = 6.3712e6 * units.m
-    omega = earth_avg_angular_vel = (2 * units.pi / units.sidereal_day).to('1/s')
+    omega = earth_avg_angular_vel = 2 * units.pi / units.sidereal_day
     d = earth_sfc_avg_dist_sun = 1.496e11 * units.m
     S = earth_solar_irradiance = units.Quantity(1.368e3, 'W / m^2')
     delta = earth_max_declination = 23.45 * units.deg
