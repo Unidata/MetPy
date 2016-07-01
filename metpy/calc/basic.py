Fix 180 degree offset in wind direction calculation

metpy/calc/basic.py
@@ -51,13 +51,14 @@ def get_wind_dir(u, v):
     Returns
     -------
     wind direction: array_like
-        The direction of the wind in degrees
+        The direction of the wind in degrees, specified as the direction from
+        which it is blowing
 
     See Also
     --------
     get_wind_components
     '''
-    wdir = 90. * units.deg - np.arctan2(v, u)
+    wdir = 270. * units.deg - np.arctan2(v, u)
     origshape = wdir.shape
     wdir = atleast_1d(wdir)
     wdir[wdir < 0] += 360. * units.deg

metpy/calc/tests/test_basic.py
@@ -52,7 +52,7 @@ def test_dir():
 
     direc = get_wind_dir(u, v)
 
-    true_dir = np.array([90., 45., 0., 90.]) * units.deg
+    true_dir = np.array([270., 225., 180., 270.]) * units.deg
 
     assert_array_almost_equal(true_dir, direc, 4)
 
@@ -65,7 +65,7 @@ def test_scalar_speed():
 
 def test_scalar_dir():
     'Test wind direction with scalars'
-    d = get_wind_dir(-3. * units('m/s'), -4. * units('m/s'))
+    d = get_wind_dir(3. * units('m/s'), 4. * units('m/s'))
     assert_almost_equal(d, 216.870 * units.deg, 3)
 
 