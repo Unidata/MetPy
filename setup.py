19af668c94938a64c4d1ef5df77c31030a6e39c9
Ryan May <rmay31@gmail.com>
2016-11-19 14:33:03 -0700

    MNT: Set up using pytest-flake8. (Fixes #239)
    This seems like a nicer, more automatic way to make sure style doesn't
    break. It works fine with flake8-quotes.

--- setup.py
+++ setup.py
@@ -90,7 +90,8 @@ setup(
         'dev': ['ipython[all]>=3.1'],
         'doc': ['sphinx>=1.3', 'ipython[all]>=3.1'],
         'examples': ['cartopy>=0.13.1'],
-        'test': ['pytest', 'pytest-runner']
+        'test': ['pytest', 'pytest-runner', 'pytest-mpl', 'pytest-flake8',
+                 'flake8-quotes', 'pep8-naming']
     },
 
     cmdclass=commands,
