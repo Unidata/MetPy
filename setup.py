96eaa24e067d76960d52f43d00728ac1af09a47f
Ryan May <rmay31@gmail.com>
2016-11-21 12:55:04 -0700

    MNT: Add flake8-import-order as a flake8-test.
    This will held standardize our imports.

--- setup.py
+++ setup.py
@@ -91,7 +91,8 @@ setup(
         'doc': ['sphinx>=1.3', 'ipython[all]>=3.1'],
         'examples': ['cartopy>=0.13.1'],
         'test': ['pytest', 'pytest-runner', 'pytest-mpl', 'pytest-flake8',
-                 'flake8>3.2.0', 'flake8-quotes', 'pep8-naming']
+                 'flake8>3.2.0', 'flake8-quotes',
+                 'flake8-import-order', 'pep8-naming']
     },
 
     cmdclass=commands,
