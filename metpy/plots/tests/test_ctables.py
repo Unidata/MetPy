9684eb70d8ce0a9377c6ce6325c6f72577935244
Ryan May <rmay31@gmail.com>
2016-11-30 17:26:01 -0700

    MNT: Refactor colortables test.
    Move away from class. Use a pytest fixture to provide an empty registry.

--- metpy/plots/tests/test_ctables.py
+++ metpy/plots/tests/test_ctables.py
@@ -13,80 +13,86 @@ except ImportError:
     buffer_args = dict(buffering=1)
 
 import numpy as np
+import pytest
 
 from metpy.plots.ctables import ColortableRegistry, convert_gempak_table
 
 
-class TestColortableRegistry(object):
-    """Test the ColortableRegistry."""
+@pytest.fixture()
+def registry():
+    """Set up a registry for use by the tests."""
+    return ColortableRegistry()
 
-    def setup_method(self, _):
-        """Set up a registry for use by the tests."""
-        self.reg = ColortableRegistry()
 
-    def test_package_resource(self):
-        """Test registry scanning package resource."""
-        self.reg.scan_resource('metpy.plots', 'nexrad_tables')
-        assert 'cc_table' in self.reg
+def test_package_resource(registry):
+    """Test registry scanning package resource."""
+    registry.scan_resource('metpy.plots', 'nexrad_tables')
+    assert 'cc_table' in registry
 
-    def test_scan_dir(self):
-        """Test registry scanning a directory."""
-        try:
-            with tempfile.NamedTemporaryFile(mode='w', dir='.', suffix='.tbl', delete=False,
-                                             **buffer_args) as fobj:
-                fobj.write('"red"\n"lime"\n"blue"\n')
-                fname = fobj.name
 
-            # Needs to be outside with so it's closed on windows
-            self.reg.scan_dir(os.path.dirname(fname))
-            name = os.path.splitext(os.path.basename(fobj.name))[0]
+def test_scan_dir(registry):
+    """Test registry scanning a directory."""
+    try:
+        with tempfile.NamedTemporaryFile(mode='w', dir='.', suffix='.tbl', delete=False,
+                                         **buffer_args) as fobj:
+            fobj.write('"red"\n"lime"\n"blue"\n')
+            fname = fobj.name
 
-            assert name in self.reg
-            assert self.reg[name] == [(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)]
-        finally:
-            os.remove(fname)
+        # Needs to be outside with so it's closed on windows
+        registry.scan_dir(os.path.dirname(fname))
+        name = os.path.splitext(os.path.basename(fobj.name))[0]
 
-    def test_read_file(self):
-        """Test reading a colortable from a file."""
-        fobj = StringIO('(0., 0., 1.0)\n"red"\n"#0000FF" #Blue')
+        assert name in registry
+        assert registry[name] == [(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)]
+    finally:
+        os.remove(fname)
 
-        self.reg.add_colortable(fobj, 'test_table')
 
-        assert 'test_table' in self.reg
-        assert self.reg['test_table'] == [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0), (0.0, 0.0, 1.0)]
+def test_read_file(registry):
+    """Test reading a colortable from a file."""
+    fobj = StringIO('(0., 0., 1.0)\n"red"\n"#0000FF" #Blue')
 
-    def test_get_colortable(self):
-        """Test getting a colortable from the registry."""
-        true_colors = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0)]
-        self.reg['table'] = true_colors
+    registry.add_colortable(fobj, 'test_table')
 
-        table = self.reg.get_colortable('table')
-        assert table.N == 2
-        assert table.colors == true_colors
+    assert 'test_table' in registry
+    assert registry['test_table'] == [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0), (0.0, 0.0, 1.0)]
 
-    def test_get_steps(self):
-        """Test getting a colortable and norm with appropriate steps."""
-        self.reg['table'] = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
-        norm, cmap = self.reg.get_with_steps('table', 5., 10.)
-        assert cmap(norm(np.array([6.]))).tolist() == [[0.0, 0.0, 1.0, 1.0]]
-        assert cmap(norm(np.array([14.9]))).tolist() == [[0.0, 0.0, 1.0, 1.0]]
-        assert cmap(norm(np.array([15.1]))).tolist() == [[1.0, 0.0, 0.0, 1.0]]
-        assert cmap(norm(np.array([26.]))).tolist() == [[0.0, 1.0, 0.0, 1.0]]
 
-    def test_get_steps_negative_start(self):
-        """Test bad start for get with steps (issue #81)."""
-        self.reg['table'] = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
-        norm, cmap = self.reg.get_with_steps('table', -10, 5)
-        assert norm.vmin == -10
-        assert norm.vmax == 5
+def test_get_colortable(registry):
+    """Test getting a colortable from the registry."""
+    true_colors = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0)]
+    registry['table'] = true_colors
 
-    def test_get_boundaries(self):
-        """Test getting a colortable with explicit boundaries."""
-        self.reg['table'] = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
-        norm, cmap = self.reg.get_with_boundaries('table', [0., 8., 10., 20.])
-        assert cmap(norm(np.array([7.]))).tolist() == [[0.0, 0.0, 1.0, 1.0]]
-        assert cmap(norm(np.array([9.]))).tolist() == [[1.0, 0.0, 0.0, 1.0]]
-        assert cmap(norm(np.array([10.1]))).tolist() == [[0.0, 1.0, 0.0, 1.0]]
+    table = registry.get_colortable('table')
+    assert table.N == 2
+    assert table.colors == true_colors
+
+
+def test_get_steps(registry):
+    """Test getting a colortable and norm with appropriate steps."""
+    registry['table'] = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
+    norm, cmap = registry.get_with_steps('table', 5., 10.)
+    assert cmap(norm(np.array([6.]))).tolist() == [[0.0, 0.0, 1.0, 1.0]]
+    assert cmap(norm(np.array([14.9]))).tolist() == [[0.0, 0.0, 1.0, 1.0]]
+    assert cmap(norm(np.array([15.1]))).tolist() == [[1.0, 0.0, 0.0, 1.0]]
+    assert cmap(norm(np.array([26.]))).tolist() == [[0.0, 1.0, 0.0, 1.0]]
+
+
+def test_get_steps_negative_start(registry):
+    """Test bad start for get with steps (issue #81)."""
+    registry['table'] = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
+    norm, cmap = registry.get_with_steps('table', -10, 5)
+    assert norm.vmin == -10
+    assert norm.vmax == 5
+
+
+def test_get_boundaries(registry):
+    """Test getting a colortable with explicit boundaries."""
+    registry['table'] = [(0.0, 0.0, 1.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
+    norm, cmap = registry.get_with_boundaries('table', [0., 8., 10., 20.])
+    assert cmap(norm(np.array([7.]))).tolist() == [[0.0, 0.0, 1.0, 1.0]]
+    assert cmap(norm(np.array([9.]))).tolist() == [[1.0, 0.0, 0.0, 1.0]]
+    assert cmap(norm(np.array([10.1]))).tolist() == [[0.0, 1.0, 0.0, 1.0]]
 
 
 def test_gempak():
