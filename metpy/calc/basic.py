BUG: Fix masking and units from windchill (Fixes #281)
Calling the direct masked_array constructor was stripping units--needed
to duplicate the logic from heat_index. Instead, refactor and add a
masked_array() function to our suite of wrappers. Also add a test
checking the units on the masked return.

metpy/calc/basic.py
@@ -13,11 +13,10 @@ These include:
 from __future__ import division
 
 import numpy as np
-from numpy.ma import masked_array
 
 from ..constants import g, omega, Rd
 from ..package_tools import Exporter
-from ..units import atleast_1d, units
+from ..units import atleast_1d, masked_array, units
 
 exporter = Exporter(globals())
 
@@ -232,7 +231,7 @@ def heat_index(temperature, rh, mask_undefined=True):
     if mask_undefined:
         mask = np.array((temperature < 80. * units.degF) | (rh < 40 * units.percent))
         if mask.any():
-            hi = units.Quantity(masked_array(hi, mask=mask), hi.units)
+            hi = masked_array(hi, mask=mask)
 
     return hi
 

metpy/units.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2008-2015 MetPy Developers.
+# Copyright (c) 2008-2016 MetPy Developers.
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
 r"""Module to provide unit support.
@@ -113,6 +113,32 @@ def atleast_2d(*arrs):
     if len(mags) == 1:
         return units.Quantity(ret, orig_units[0])
     return [units.Quantity(m, u) for m, u in zip(ret, orig_units)]
+
+
+def masked_array(data, data_units=None, **kwargs):
+    """Create a :class:`numpy.ma.MaskedArray` with units attached.
+
+    This is a thin wrapper around :func:`numpy.ma.masked_array` that ensures that
+    units are properly attached to the result (otherwise units are silently lost). Units
+    are taken from the ``units`` argument, or if this is ``None``, the units on ``data``
+    are used.
+
+    Parameters
+    ----------
+    data : array_like
+        The source data. If ``units`` is `None`, this should be a `pint.Quantity` with
+        the desired units.
+    data_units : str or `pint.Unit`
+        The units for the resulting `pint.Quantity`
+    **kwargs : Arbitrary keyword arguments passed to `numpy.ma.masked_array`
+
+    Returns
+    -------
+    `pint.Quantity`
+    """
+    if data_units is None:
+        data_units = data.units
+    return units.Quantity(np.ma.masked_array(data, **kwargs), data_units)
 
 
 del pint