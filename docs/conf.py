0212639ff88a741fb53bc4bf289f772a4b269e99
Ryan May <rmay31@gmail.com>
2017-01-06 11:03:43 -0700

    ENH: Add build date to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,7 @@ release = verinfo
 # non-false value, then it is used:
 # today = ''
 # Else, today_fmt is used as the format for a strftime call.
-# today_fmt = '%B %d, %Y'
+today_fmt = '%B %d, %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -186,7 +186,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y at %H:%M:%S'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
