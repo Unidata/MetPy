c5e12239e2f026fea8fe8c3c074c5aa0e5c391f8
J.R. Leeman <kd5wxb@gmail.com>
2017-02-09 11:12:24 -0700

    Add symbols to constants docstring

--- metpy/constants.py
+++ metpy/constants.py
@@ -1,73 +1,62 @@
 # Copyright (c) 2008-2015 MetPy Developers.
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
-r"""
-This is a collection of meteorologically significant constants.
+
+r"""This is a collection of meteorologically significant constants. # noqa: E501.
 
 Earth
 -----
-======================== ===== ======== =======================================
-Name                     Abbr. Units    Description
------------------------- ----- -------- ---------------------------------------
-earth_avg_radius         Re    m        Avg. radius of the Earth
-earth_gravity            g     m s^-2   Avg. gravity acceleration on Earth
-earth_avg_angular_vel    omega rad s^-1 Avg. angular velocity of Earth
-earth_sfc_avg_dist_sun   d     m        Avg. distance of the Earth from the Sun
-earth_solar_irradiance   S     W m^-2   Avg. solar irradiance of Earth
-earth_max_declination    delta degrees  Max. solar declination angle of Earth
-earth_orbit_eccentricity       None     Avg. eccentricity of Earth's orbit
-======================== ===== ======== =======================================
+======================== =============== =========== ========================== =======================================
+Name                     Symbol          Short Name  Units                      Description
+------------------------ --------------- ----------- -------------------------- ---------------------------------------
+earth_avg_radius         :math:`R_e`     Re          :math:`\text{m}`           Avg. radius of the Earth
+earth_gravity            :math:`g`       g           :math:`\text{m s}^{-2}`    Avg. gravity acceleration on Earth
+earth_avg_angular_vel    :math:`\Omega`  omega       :math:`\text{rad s}^{-1}`  Avg. angular velocity of Earth
+earth_sfc_avg_dist_sun   :math:`d`       d           :math:`\text{m}`           Avg. distance of the Earth from the Sun
+earth_solar_irradiance   :math:`S`       S           :math:`\text{W m}^{-2}`    Avg. solar irradiance of Earth
+earth_max_declination    :math:`\delta`  delta       :math:`\text{degrees}`     Max. solar declination angle of Earth
+earth_orbit_eccentricity :math:`e`                   :math:`\text{None}`        Avg. eccentricity of Earth's orbit
+======================== =============== =========== ========================== =======================================
 
 Water
 -----
-======================= ===== =========== =====================================
-Name                    Abbr. Units       Description
------------------------ ----- ----------- -------------------------------------
-water_molecular_weight  Mw    g mol^-1    Molecular weight of water
-water_gas_constant      Rv    J (K kg)^-1 Gas constant for water vapor
-density_water           rho_l kg m^-3     Nominal density of liquid water at 0C
-wv_specific_heat_press  Cp_v  J (K kg)^-1 Specific heat at constant pressure
-                                          for water vapor
-wv_specific_heat_vol    Cv_v  J (K kg)^-1 Specific heat at constant volume for
-                                          water vapor
-water_specific_heat     Cp_l  J (K kg)^-1 Specific heat of liquid water at 0C
-water_heat_vaporization Lv    J kg^-1     Latent heat of vaporization for
-                                          liquid water at 0C
-water_heat_fusion       Lf    J kg^-1     Latent heat of fusion for liquid
-                                          water at 0C
-ice_specific_heat       Cp_i  J (K kg)^-1 Specific heat of ice at 0C
-density_ice             rho_i kg m^-3     Density of ice at 0C
-======================= ===== =========== =====================================
+======================= ================ ========== ============================ ====================================================
+Name                    Symbol           Short Name Units                        Description
+----------------------- ---------------- ---------- ---------------------------- ----------------------------------------------------
+water_molecular_weight  :math:`M_w`      Mw         :math:`\text{g mol}^{-1}`    Molecular weight of water
+water_gas_constant      :math:`R_v`      Rv         :math:`\text{J (K kg)}^{-1}` Gas constant for water vapor
+density_water           :math:`\rho_l`   rho_l      :math:`\text{kg m}^{-3}`     Nominal density of liquid water at 0C
+wv_specific_heat_press  :math:`C_{pv}`   Cp_v       :math:`\text{J (K kg)}^{-1}` Specific heat at constant pressure for water vapor
+wv_specific_heat_vol    :math:`C_{vv}`   Cv_v       :math:`\text{J (K kg)}^{-1}` Specific heat at constant volume for water vapor
+water_specific_heat     :math:`Cp_l`     Cp_l       :math:`\text{J (K kg)}^{-1}` Specific heat of liquid water at 0C
+water_heat_vaporization :math:`L_v`      Lv         :math:`\text{J kg}^{-1}`     Latent heat of vaporization for liquid water at 0C
+water_heat_fusion       :math:`L_f`      Lf         :math:`\text{J kg}^{-1}`     Latent heat of fusion for liquid water at 0C
+ice_specific_heat       :math:`C_{pi}`   Cp_i       :math:`\text{J (K kg)}^{-1}` Specific heat of ice at 0C
+density_ice             :math:`\rho_i`   rho_i      :math:`\text{kg m}^{-3}`     Density of ice at 0C
+======================= ================ ========== ============================ ====================================================
 
 Dry Air
 -------
-======================== ===== =========== ====================================
-Name                     Abbr. Units       Description
------------------------- ----- ----------- ------------------------------------
-dry_air_molecular_weight Md    g / mol     Nominal molecular weight of dry air
-                                           at the surface of th Earth
-dry_air_gas_constant     Rd    J (K kg)^-1 Gas constant for dry air at the
-                                           surface of the Earth
-dry_air_spec_heat_press  Cp_d  J (K kg)^-1 Specific heat at constant pressure
-                                           for dry air
-dry_air_spec_heat_vol    Cv_d  J (K kg)^-1 Specific heat at constant volume
-                                           for dry air
-dry_air_density_stp      rho_d kg m^-3     Density of dry air at 0C and 1000mb
-======================== ===== =========== ====================================
+======================== ================ ============= ============================ ===============================================================
+Name                     Symbol           Short Name    Units                        Description
+------------------------ ---------------- ------------- ---------------------------- ---------------------------------------------------------------
+dry_air_molecular_weight :math:`M_d`      Md            :math:`\text{g / mol}`       Nominal molecular weight of dry air at the surface of th Earth
+dry_air_gas_constant     :math:`R_d`      Rd            :math:`\text{J (K kg)}^{-1}` Gas constant for dry air at the surface of the Earth
+dry_air_spec_heat_press  :math:`C_{pd}`   Cp_d          :math:`\text{J (K kg)}^{-1}` Specific heat at constant pressure for dry air
+dry_air_spec_heat_vol    :math:`C_{vd}`   Cv_d          :math:`\text{J (K kg)}^{-1}` Specific heat at constant volume for dry air
+dry_air_density_stp      :math:`\rho_d`   rho_d         :math:`\text{kg m}^{-3}`     Density of dry air at 0C and 1000mb
+======================== ================ ============= ============================ ===============================================================
 
 General Meteorology Constants
 -----------------------------
-======================== ======= ======== =====================================
-Name                     Abbr.   Units    Description
------------------------- ------- -------- -------------------------------------
-pot_temp_ref_press       P0      Pa       Reference pressure for potential
-                                          temperature
-poisson_exponent         kappa   None     Exponent in Poisson's equation
-                                          (Rd/Cp_d)
-dry_adiabatic_lapse_rate gamma_d K km^-1  The dry adiabatic lapse rate
-molecular_weight_ratio   epsilon None     Ratio of molecular weight of water to
-                                          that of dry air
-======================== ======= ======== =====================================
+======================== ================= =========== ========================= =======================================================
+Name                     Symbol            Short Name   Units                    Description
+------------------------ ----------------- ----------- ------------------------- -------------------------------------------------------
+pot_temp_ref_press       :math:`P_0`       P0          :math:`\text{Pa}`         Reference pressure for potential temperature
+poisson_exponent         :math:`\kappa`    kappa       :math:`\text{None}`       Exponent in Poisson's equation (Rd/Cp_d)
+dry_adiabatic_lapse_rate :math:`\gamma_d`  gamma_d     :math:`\text{K km}^{-1}`  The dry adiabatic lapse rate
+molecular_weight_ratio   :math:`\epsilon`  epsilon     :math:`\text{None}`       Ratio of molecular weight of water to that of dry air
+======================== ================= =========== ========================= =======================================================
 """
 
 from .package_tools import Exporter
