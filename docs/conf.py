d7b82acb2dc2318df5218b88be3c9912201b654d
Ryan May <rmay@ucar.edu>
2017-03-15 17:21:20 -0600

    ENH: Add MetPy logo to docs.
    This uses built-in theme support. We also add a favicon.

--- docs/conf.py
+++ docs/conf.py
@@ -178,12 +178,12 @@ def setup(app):
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = None
+html_logo = '_static/metpy_150x150.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# html_favicon = None
+html_favicon = '_static/metpy_32x32.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
