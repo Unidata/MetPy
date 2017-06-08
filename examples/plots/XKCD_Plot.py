a5c5c1c1bfcf7fe5fe456aa3502139f54df1ada8
J.R. Leeman <kd5wxb@gmail.com>
2017-06-08 10:12:32 -0500

    Add simple XKCD example. DO NOT MERGE

---
+++ examples/plots/XKCD_Plot.py
@@ -0,0 +1,52 @@
+"""
+Stove Ownership
+===============
+
+Make a very important plot in the XKCD style.
+
+This is a simple example PR.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+###########################################
+# The setup
+# ---------
+#
+# First we setup a figure
+# Based on "Stove Ownership" from XKCD by Randall Monroe
+# http://xkcd.com/418/
+
+plt.xkcd()
+fig = plt.figure()
+ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+plt.xticks([])
+plt.yticks([])
+ax.set_ylim([-30, 10])
+
+data = np.ones(100)
+data[70:] -= np.arange(30)
+
+plt.annotate(
+    'THE DAY I REALIZED\nI COULD COOK BACON\nWHENEVER I WANTED',
+    xy=(70, 1), arrowprops=dict(arrowstyle='->'), xytext=(15, -10))
+
+plt.plot(data)
+
+plt.xlabel('time')
+plt.ylabel('my overall health')
+fig.text(
+    0.5, 0.05,
+    '"Stove Ownership" from xkcd by Randall Monroe',
+    ha='center')
+
+plt.show()
+
+###########################################
+# More code could go here
+data = np.ones(100)
+data[70:] -= np.arange(30)
