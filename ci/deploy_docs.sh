18ac56a40f4d2c6fd4a92ee325301547f8400a04
Ryan May <rmay@ucar.edu>
2017-07-01 15:49:47 -0600

    ENH: Move back to custom script for doc deployment
    This allows us to start deploying versioned docs.

---
+++ ci/deploy_docs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# Clone *this* git repo, but only the gh-pages branch. We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+if [[ ! -d $GH_PAGES_DIR ]]; then
+    git clone -q -b gh-pages --single-branch https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git $GH_PAGES_DIR 2>&1 >/dev/null
+fi
+cd $GH_PAGES_DIR
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "travis@nobody.org"
+
+if [[ "${TRAVIS_TAG}" != "" ]]; then
+    export VERSION=${TRAVIS_TAG%.*}
+else
+    export VERSION=dev
+fi
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+rm -rf ${VERSION}
+cp -R ${TRAVIS_BUILD_DIR}/docs/build/html/ ${VERSION}/
+touch .nojekyll
+if [[ "${VERSION}" != "dev" ]]; then
+    ln -shf ${VERSION} latest
+fi
+
+git add -A .
+git commit -m "Deploy ${VERSION} to GitHub Pages"
+
+# Push up to gh-pages
+echo Pushing...
+git push --quiet origin gh-pages
