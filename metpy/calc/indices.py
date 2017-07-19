9347a07fd1865328bc778ff31ed7217930081147
Matt Wilson <matthew.wilson@valpo.edu>
2017-07-19 10:25:06 -0500

    Added a function to calculate pressure-weighted mean wind in a layer.

--- metpy/calc/indices.py
+++ metpy/calc/indices.py
@@ -51,3 +51,61 @@ def precipitable_water(dewpt, pressure, top=400 * units('hPa')):
     pw = -1. * (np.trapz(w.magnitude, pres_layer.magnitude) * (w.units * pres_layer.units) /
                 (g * rho_l))
     return pw.to('millimeters')
+
+
+def mean_wind_pressure_weighted(u, v, p, hgt, top, bottom=None, interp=False):
+    r"""Calculate pressure-weighted mean wind through a layer.
+
+    Layer top and bottom specified in meters AGL.
+
+    Parameters
+    ----------
+    u : array-like
+        U-component of wind.
+    v : array-like
+        V-component of wind.
+    p : array-like
+        Atmospheric pressure profile
+    hgt : array-like
+        Heights from sounding
+    top: `pint.Quantity`
+        The top of the layer in meters AGL
+    bottom: `pint.Quantity`
+        The bottom of the layer in meters AGL.
+        Default is the surface.
+
+    Returns
+    -------
+    `pint.Quantity`
+        u_mean: u-component of layer mean wind, in m/s
+    `pint.Quantity`
+        v_mean: v-component of layer mean wind, in m/s
+
+    """
+    u = u.to('meters/second')
+    v = v.to('meters/second')
+
+    if bottom:
+        depth_s = top - bottom
+        bottom = bottom + hgt[0]
+    else:
+        depth_s = top
+
+    if interp:
+        dp = -1
+        pressure_top = np.interp(top.magnitude, hgt.magnitude - hgt[0].magnitude,
+                                 np.log(p.magnitude))
+        pressure_top = np.exp(pressure_top)
+        interp_levels = (np.arange(p[0].magnitude, pressure_top + dp, dp)) * units('hPa')
+        u_int = log_interp(interp_levels, p, u)
+        v_int = log_interp(interp_levels, p, v)
+        h_int = log_interp(interp_levels, p, hgt)
+        w_int = get_layer(interp_levels, u_int, v_int, heights=h_int,
+                          bottom=bottom, depth=depth_s)
+    else:
+        w_int = get_layer(p, u, v, heights=hgt, bottom=bottom, depth=depth_s)
+
+    u_mean = ma.average(w_int[1], weights = w_int[0]) * units('m/s')
+    v_mean = ma.average(w_int[2], weights = w_int[0]) * units('m/s')
+
+    return u_mean, v_mean
