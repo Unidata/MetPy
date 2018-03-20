fa4c312c43ed8ee738453be755df3bc403a1b4d8
John Leeman <kd5wxb@gmail.com>
2018-03-20 16:42:21 -0600

    Change testing for newer test data retreival.

--- metpy/calc/tests/test_indices.py
+++ metpy/calc/tests/test_indices.py
@@ -11,9 +11,7 @@ import numpy as np
 from metpy.calc import (bulk_shear, bunkers_storm_motion, mean_pressure_weighted,
                         precipitable_water, significant_tornado, supercell_composite)
 from metpy.deprecation import MetpyDeprecationWarning
-from metpy.io import get_upper_air_data
-from metpy.io.upperair import UseSampleData
-from metpy.testing import assert_almost_equal, assert_array_equal
+from metpy.testing import assert_almost_equal, assert_array_equal, get_upper_air_data
 from metpy.units import concatenate, units
 
 warnings.simplefilter('ignore', MetpyDeprecationWarning)
@@ -21,9 +19,8 @@ warnings.simplefilter('ignore', MetpyDeprecationWarning)
 
 def test_precipitable_water():
     """Test precipitable water with observed sounding."""
-    with UseSampleData():
-        data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC', source='wyoming')
-    pw = precipitable_water(data.variables['dewpoint'][:], data.variables['pressure'][:],
+    data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC')
+    pw = precipitable_water(data['dewpoint'], data['pressure'],
                             top=400 * units.hPa)
     truth = (0.8899441949243486 * units('inches')).to('millimeters')
     assert_array_equal(pw, truth)
@@ -31,10 +28,9 @@ def test_precipitable_water():
 
 def test_precipitable_water_no_bounds():
     """Test precipitable water with observed sounding and no bounds given."""
-    with UseSampleData():
-        data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC', source='wyoming')
-    dewpoint = data.variables['dewpoint'][:]
-    pressure = data.variables['pressure'][:]
+    data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC')
+    dewpoint = data['dewpoint']
+    pressure = data['pressure']
     inds = pressure >= 400 * units.hPa
     pw = precipitable_water(dewpoint[inds], pressure[inds])
     truth = (0.8899441949243486 * units('inches')).to('millimeters')
@@ -54,12 +50,11 @@ def test_precipitable_water_bound_error():
 
 def test_mean_pressure_weighted():
     """Test pressure-weighted mean wind function with vertical interpolation."""
-    with UseSampleData():
-        data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC', source='wyoming')
-    u, v = mean_pressure_weighted(data.variables['pressure'][:],
-                                  data.variables['u_wind'][:],
-                                  data.variables['v_wind'][:],
-                                  heights=data.variables['height'][:],
+    data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC')
+    u, v = mean_pressure_weighted(data['pressure'],
+                                  data['u_wind'],
+                                  data['v_wind'],
+                                  heights=data['height'],
                                   depth=6000 * units('meter'))
     assert_almost_equal(u, 6.0208700094534775 * units('m/s'), 7)
     assert_almost_equal(v, 7.966031839967931 * units('m/s'), 7)
@@ -67,25 +62,23 @@ def test_mean_pressure_weighted():
 
 def test_mean_pressure_weighted_elevated():
     """Test pressure-weighted mean wind function with a base above the surface."""
-    with UseSampleData():
-        data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC', source='wyoming')
-    u, v = mean_pressure_weighted(data.variables['pressure'][:],
-                                  data.variables['u_wind'][:],
-                                  data.variables['v_wind'][:],
-                                  heights=data.variables['height'][:],
+    data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC')
+    u, v = mean_pressure_weighted(data['pressure'],
+                                  data['u_wind'],
+                                  data['v_wind'],
+                                  heights=data['height'],
                                   depth=3000 * units('meter'),
-                                  bottom=data.variables['height'][0] + 3000 * units('meter'))
+                                  bottom=data['height'][0] + 3000 * units('meter'))
     assert_almost_equal(u, 8.270829843626476 * units('m/s'), 7)
     assert_almost_equal(v, 1.7392601775853547 * units('m/s'), 7)
 
 
 def test_bunkers_motion():
     """Test Bunkers storm motion with observed sounding."""
-    with UseSampleData():
-        data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC', source='wyoming')
-    motion = concatenate(bunkers_storm_motion(data.variables['pressure'][:],
-                         data.variables['u_wind'][:], data.variables['v_wind'][:],
-                         data.variables['height'][:]))
+    data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC')
+    motion = concatenate(bunkers_storm_motion(data['pressure'],
+                         data['u_wind'], data['v_wind'],
+                         data['height']))
     truth = [1.4537892577864744, 2.0169333025630616, 10.587950761120482, 13.915130377372801,
              6.0208700094534775, 7.9660318399679308] * units('m/s')
     assert_almost_equal(motion.flatten(), truth, 8)
@@ -93,10 +86,9 @@ def test_bunkers_motion():
 
 def test_bulk_shear():
     """Test bulk shear with observed sounding."""
-    with UseSampleData():
-        data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC', source='wyoming')
-    u, v = bulk_shear(data.variables['pressure'][:], data.variables['u_wind'][:],
-                      data.variables['v_wind'][:], heights=data.variables['height'][:],
+    data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC')
+    u, v = bulk_shear(data['pressure'], data['u_wind'],
+                      data['v_wind'], heights=data['height'],
                       depth=6000 * units('meter'))
     truth = [29.899581266946115, -14.389225800205509] * units('knots')
     assert_almost_equal(u.to('knots'), truth[0], 8)
@@ -105,10 +97,9 @@ def test_bulk_shear():
 
 def test_bulk_shear_no_depth():
     """Test bulk shear with observed sounding and no depth given. Issue #568."""
-    with UseSampleData():
-        data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC', source='wyoming')
-    u, v = bulk_shear(data.variables['pressure'][:], data.variables['u_wind'][:],
-                      data.variables['v_wind'][:], heights=data.variables['height'][:])
+    data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC')
+    u, v = bulk_shear(data['pressure'], data['u_wind'],
+                      data['v_wind'], heights=data['height'])
     truth = [20.225018939, 22.602359692] * units('knots')
     assert_almost_equal(u.to('knots'), truth[0], 8)
     assert_almost_equal(v.to('knots'), truth[1], 8)
@@ -116,11 +107,10 @@ def test_bulk_shear_no_depth():
 
 def test_bulk_shear_elevated():
     """Test bulk shear with observed sounding and a base above the surface."""
-    with UseSampleData():
-        data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC', source='wyoming')
-    u, v = bulk_shear(data.variables['pressure'][:], data.variables['u_wind'][:],
-                      data.variables['v_wind'][:], heights=data.variables['height'][:],
-                      bottom=data.variables['height'][0] + 3000 * units('meter'),
+    data = get_upper_air_data(datetime(2016, 5, 22, 0), 'DDC')
+    u, v = bulk_shear(data['pressure'], data['u_wind'],
+                      data['v_wind'], heights=data['height'],
+                      bottom=data['height'][0] + 3000 * units('meter'),
                       depth=3000 * units('meter'))
     truth = [0.9655943923302139, -3.8405428777944466] * units('m/s')
     assert_almost_equal(u, truth[0], 8)
