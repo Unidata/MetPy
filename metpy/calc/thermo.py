e61befc237c968bd2d9e995ade52f38bfc8f5f37
J.R. Leeman <kd5wxb@gmail.com>
2018-05-03 09:55:35 -0600

    Refactor layer calculation

--- metpy/calc/thermo.py
+++ metpy/calc/thermo.py
@@ -2156,18 +2156,16 @@ def downdraft_cape(pressure, temperature, dewpoint, parcel=None, bottom=None,
     # If the user specified a parcel starting point, we'll use that instead of the default
     if parcel:
         parcel_starting_pressure, parcel_starting_temperature = parcel
-        print(np.nanmax(pressure) * pressure.units - parcel_starting_pressure)
+        profile_depth = np.nanmax(pressure) * pressure.units - parcel_starting_pressure
+
         # Trim data to parcel starting point and below, interpolating the starting point
-        pressure, temperature, dewpoint = get_layer(pressure, temperature, dewpoint,
-                                                    depth=(np.nanmax(pressure) * pressure.units -
-                                                           parcel_starting_pressure),
-                                                    heights=heights)
+        pressure, temperature = get_layer(pressure, temperature,
+                                          depth= profile_depth, heights=heights)
 
         # Trim data to be only above the bottom, interpolating if necessary. This must be done
         # in separate step as there is no way to specify the top of a layer, just depth.
-        pressure, temperature, dewpoint = get_layer(pressure, temperature, dewpoint,
-                                                    bottom=bottom,
-                                                    heights=heights)
+        pressure, temperature = get_layer(pressure, temperature, bottom=bottom,
+                                          depth= profile_depth, heights=heights)
 
     # The user did not give us a parcel, so we'll calculate a sensible default.
     else:
