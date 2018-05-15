MNT: Fix close test functions with mixed units and not
This was triggering a warning about numpy.float64 and a lack of a _units
attribute. The root cause was comparing a value with units with one that
had them. One fix is to make sure neither have units in parcel profile
(since both values have been coerced already to have the same).

The other change is to tweak _greater_or_close and _less_or_close to use
> and < rather than numpy.greater and numpy.less, so that an error
results if a Quantity and an array are compared.

metpy/calc/thermo.py
@@ -467,7 +467,7 @@ def parcel_profile(pressure, temperature, dewpt):
     t1 = dry_lapse(press_lower, temperature)
 
     # If the pressure profile doesn't make it to the lcl, we can stop here
-    if _greater_or_close(np.nanmin(pressure), lcl_pressure):
+    if _greater_or_close(np.nanmin(pressure), lcl_pressure.m):
         return t1[:-1]
 
     # Find moist pseudo-adiabatic profile starting at the LCL