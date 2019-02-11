eda7b8598005d98b1ae89c17cc2eb487cd0e7f30
J.R. Leeman <kd5wxb@gmail.com>
2019-02-11 13:21:45 -0700

    [MNT] Bump to numpy 1.12

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
                   'metpy': ['static-data-manifest.txt']},
 
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
-    install_requires=['matplotlib>=2.0.0', 'numpy>=1.11.0', 'scipy>=0.17.0',
+    install_requires=['matplotlib>=2.0.0', 'numpy>=1.12.0', 'scipy>=0.17.0',
                       'pint!=0.9', 'xarray>=0.10.7', 'enum34;python_version<"3.4"',
                       'pooch>=0.1, <0.3', 'traitlets>=4.3.0'],
     extras_require={
