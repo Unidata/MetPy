b7b8ac93d61b009439287406919b79b47e7a7878
akrherz <akrherz@iastate.edu>
2019-05-03 09:28:27 -0500

    allow any pooch >= 0.1 to be used

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     install_requires=['matplotlib>=2.0.0', 'numpy>=1.12.0', 'scipy>=0.17.0',
                       'pint!=0.9', 'xarray>=0.10.7', 'enum34;python_version<"3.4"',
                       'contextlib2;python_version<"3.6"',
-                      'pooch>=0.1, <0.3', 'traitlets>=4.3.0'],
+                      'pooch>=0.1', 'traitlets>=4.3.0'],
     extras_require={
         'dev': ['ipython[all]>=3.1'],
         'doc': ['sphinx>=1.4', 'sphinx-gallery', 'doc8', 'm2r',
