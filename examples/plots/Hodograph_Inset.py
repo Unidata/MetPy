41eb0fac0867ce8c002ebc77104a7f883a0e33a7
Igor Oliveira <igor.oliveira@meteogroup.com>
2019-07-27 11:31:47 +0200

    Add/modify Hodograph plot_colormapped examples #752
    correct import statements
    
    Solving format issues: double->single quotes
    
    Solving format issues: double->single quotes
    
    Import statements reorder
    
    Update examples/plots/Hodograph_Inset.py
    
    Co-Authored-By: Zach Bruick <zachary.bruick@valpo.edu>

--- examples/plots/Hodograph_Inset.py
+++ examples/plots/Hodograph_Inset.py
@@ -8,6 +8,7 @@ Hodograph Inset
 Layout a Skew-T plot with a hodograph inset into the plot.
 """
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import numpy as np
@@ -71,9 +72,16 @@ skew.ax.set_xlim(-50, 60)
 
 # Create a hodograph
 ax_hod = inset_axes(skew.ax, '40%', '40%', loc=1)
-h = Hodograph(ax_hod, component_range=80.)
+h = Hodograph(ax_hod, component_range=80.0)
 h.add_grid(increment=20)
-h.plot_colormapped(u, v, np.hypot(u, v))
+# create array of data to use for colormapping
+c = np.hypot(u, v)
+# Pass in a custom norm and colormap (from mpl.cm)
+norm = mpl.colors.Normalize(vmin=c.min(), vmax=c.max())
+colors = [mpl.cm.viridis(norm(level)) for level in c]
 
+# Create and show plot
+h.plot_colormapped(u, v, c, bounds=c[:-1], colors=colors)
 # Show the plot
 plt.show()
+#
