4b40487520aef5a6777d1f615469e9c1d0a9c9b7
Igor Oliveira <igor.oliveira@meteogroup.com>
2019-07-27 16:38:53 +0200

    Solving format issues: double->single quotes

--- examples/plots/Hodograph_Inset.py
+++ examples/plots/Hodograph_Inset.py
@@ -8,8 +8,8 @@ Hodograph Inset
 Layout a Skew-T plot with a hodograph inset into the plot.
 """
 
-import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import numpy as np
 import pandas as pd
@@ -23,48 +23,27 @@ from metpy.units import units
 # Upper air data can be obtained using the siphon package, but for this example we will use
 # some of MetPy's sample data.
 
-col_names = [
-    "pressure",
-    "height",
-    "temperature",
-    "dewpoint",
-    "direction",
-    "speed",
-]
+col_names = ['pressure', 'height', 'temperature', 'dewpoint', 'direction', 'speed']
 
-df = pd.read_fwf(
-    get_test_data("may4_sounding.txt", as_file_obj=False),
-    skiprows=5,
-    usecols=[0, 1, 2, 3, 6, 7],
-    names=col_names,
-)
+df = pd.read_fwf(get_test_data('may4_sounding.txt', as_file_obj=False),
+                 skiprows=5, usecols=[0, 1, 2, 3, 6, 7], names=col_names)
 
-df["u_wind"], df["v_wind"] = mpcalc.wind_components(
-    df["speed"], np.deg2rad(df["direction"])
-)
+df['u_wind'], df['v_wind'] = mpcalc.wind_components(df['speed'],
+                                                    np.deg2rad(df['direction']))
 
 # Drop any rows with all NaN values for T, Td, winds
-df = df.dropna(
-    subset=(
-        "temperature",
-        "dewpoint",
-        "direction",
-        "speed",
-        "u_wind",
-        "v_wind",
-    ),
-    how="all",
-).reset_index(drop=True)
+df = df.dropna(subset=('temperature', 'dewpoint', 'direction', 'speed',
+                       'u_wind', 'v_wind'), how='all').reset_index(drop=True)
 
 ###########################################
 # We will pull the data out of the example dataset into individual variables and
 # assign units.
 
-p = df["pressure"].values * units.hPa
-T = df["temperature"].values * units.degC
-Td = df["dewpoint"].values * units.degC
-wind_speed = df["speed"].values * units.knots
-wind_dir = df["direction"].values * units.degrees
+p = df['pressure'].values * units.hPa
+T = df['temperature'].values * units.degC
+Td = df['dewpoint'].values * units.degC
+wind_speed = df['speed'].values * units.knots
+wind_dir = df['direction'].values * units.degrees
 u, v = mpcalc.wind_components(wind_speed, wind_dir)
 
 ###########################################
@@ -78,8 +57,8 @@ skew = SkewT(fig, rotation=45)
 
 # Plot the data using normal plotting functions, in this case using
 # log scaling in Y, as dictated by the typical meteorological plot
-skew.plot(p, T, "r")
-skew.plot(p, Td, "g")
+skew.plot(p, T, 'r')
+skew.plot(p, Td, 'g')
 skew.plot_barbs(p, u, v)
 skew.ax.set_ylim(1000, 100)
 
