Fix for skewt subroutines ignoring xaxis units
Wet and dry adiabats, along with mixing lines, do not correctly
plot unless everything is in degrees celcius. This is especially
a problem when the plots show the wrong values without giving
any sort of warning.

Closes #1155

metpy/plots/skewt.py
@@ -416,6 +416,30 @@ class SkewT(object):
         b.set_clip_box(transforms.TransformedBbox(ax_bbox, self.ax.transAxes))
         return b
 
+    def get_t0(self, spacing=10):
+        r"""
+        
+        Return range of temperatures based on xaxis
+        Parameters
+        ----------
+        spacing : int or float, optional
+            Space between returned temperature values
+        
+        Returns
+        -------
+        units.Quantity 
+        """
+        # minimum and maximum temperature
+        xmin, xmax = self.ax.get_xlim()
+        
+        # units if they were supplied are stored in the xaxis
+        tunits = self.ax.xaxis.units
+        
+        # if temperature has no units, guess from the lower bound
+        if tunits is None:
+            tunits = [units.degC, units.degK][xmin > 150]
+        return np.arange(xmin, xmax + 1, spacing) * tunits
+    
     def plot_dry_adiabats(self, t0=None, p=None, **kwargs):
         r"""Plot dry adiabats.
 
@@ -426,7 +450,7 @@ class SkewT(object):
         Parameters
         ----------
         t0 : array_like, optional
-            Starting temperature values in Kelvin. If none are given, they will be
+            Starting temperature values. If none are given, they will be
             generated using the current temperature range at the bottom of
             the plot.
         p : array_like, optional
@@ -450,15 +474,14 @@ class SkewT(object):
         """
         # Determine set of starting temps if necessary
         if t0 is None:
-            xmin, xmax = self.ax.get_xlim()
-            t0 = np.arange(xmin, xmax + 1, 10) * units.degC
+            t0 = self.get_t0()
 
         # Get pressure levels based on ylims if necessary
         if p is None:
             p = np.linspace(*self.ax.get_ylim()) * units.mbar
 
         # Assemble into data for plotting
-        t = dry_lapse(p, t0[:, np.newaxis], 1000. * units.mbar).to(units.degC)
+        t = dry_lapse(p, t0[:, np.newaxis], 1000. * units.mbar).to(t0.units)
         linedata = [np.vstack((ti, p)).T for ti in t]
 
         # Add to plot
@@ -502,16 +525,14 @@ class SkewT(object):
         """
         # Determine set of starting temps if necessary
         if t0 is None:
-            xmin, xmax = self.ax.get_xlim()
-            t0 = np.concatenate((np.arange(xmin, 0, 10),
-                                 np.arange(0, xmax + 1, 5))) * units.degC
+            t0 = self.get_t0()
 
         # Get pressure levels based on ylims if necessary
         if p is None:
             p = np.linspace(*self.ax.get_ylim()) * units.mbar
 
         # Assemble into data for plotting
-        t = moist_lapse(p, t0[:, np.newaxis], 1000. * units.mbar).to(units.degC)
+        t = moist_lapse(p, t0[:, np.newaxis], 1000. * units.mbar).to(t0.units)
         linedata = [np.vstack((ti, p)).T for ti in t]
 
         # Add to plot
@@ -558,8 +579,12 @@ class SkewT(object):
         if p is None:
             p = np.linspace(600, max(self.ax.get_ylim())) * units.mbar
 
-        # Assemble data for plotting
+        # Dewpoint contours calculated and converted to xaxis units
         td = dewpoint(vapor_pressure(p, w))
+        if self.ax.xaxis.units is not None:
+            td = td.to(self.ax.xaxis.units)
+
+        # Assemble data for plotting
         linedata = [np.vstack((t, p)).T for t in td]
 
         # Add to plot