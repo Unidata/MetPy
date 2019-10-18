4c42229d9a1175b7dd46fe03789fb488c7205a5a
zbruick <zachary.bruick@valpo.edu>
2019-10-18 10:10:57 -0600

    Add ImportError to __init__.py if python2 is detected

--- src/metpy/__init__.py
+++ src/metpy/__init__.py
@@ -4,8 +4,27 @@
 """Tools for reading, calculating, and plotting with weather data."""
 
 # What do we want to pull into the top-level namespace?
-
+import sys
 import warnings
+
+if sys.version_info < (3,):
+    raise ImportError(
+        """You are running MetPy 0.12.0 or greater on Python 2.
+
+        MetPy 0.12.0 and above are no longer compatible with Python 2, but this version was
+        still installed. Sorry about that; it should not have happened. Make sure you have
+        pip >= 9.0 to avoid this kind of issue, as well as setuptools >= 24.2:
+
+        $ pip install pip setuptools --upgrade
+
+        Your choices:
+
+        - Upgrade to Python 3.
+
+        - Install an older version of MetPy:
+
+        $ pip install 'metpy=0.11.1'
+        """)
 
 # Must occur before below imports
 warnings.filterwarnings('ignore', 'numpy.dtype size changed')
