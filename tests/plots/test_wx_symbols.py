79d528b2cf6d21068c5d8427317717f009b300e5
kgoebber <kevin.goebbert@valpo.edu>
2019-11-18 11:45:00 -0600

    Adds function to get numeric wx code

--- tests/plots/test_wx_symbols.py
+++ tests/plots/test_wx_symbols.py
@@ -2,7 +2,10 @@
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
 """Tests for the `wx_symbols` module."""
-from metpy.plots import current_weather
+import numpy as np
+
+from metpy.plots import current_weather, wx_code_to_numeric
+from metpy.testing import assert_array_equal
 
 
 def test_mapper():
@@ -22,3 +25,21 @@ def test_alt_char():
 def test_mapper_len():
     """Test getting the length of the mapper."""
     assert len(current_weather) == 100
+
+
+def test_wx_code_to_numeric():
+    """Test getting numeric weather codes from METAR."""
+    data = ['SN', '-RA', '-SHSN', '-SHRA', 'DZ', 'RA', 'SHSN', 'TSRA', '-FZRA', '-SN', '-TSRA',
+            '-RASN', '+SN', 'FG', '-SHRASN', '-DZ', 'SHRA', '-FZRASN', 'TSSN', 'MIBCFG',
+            '-RAPL', 'RAPL', 'TSSNPL', '-SNPL', '+RA', '-RASNPL', '-BLSN', '-SHSNIC', '+TSRA',
+            'TS', 'PL', 'SNPL', '-SHRAPL', '-SNSG', '-TSSN', 'SG', 'IC', 'FU', '+SNPL',
+            'TSSNPLGR', '-TSSNPLGR', '-SHSNSG', 'SHRAPL', '-TSRASN', 'FZRA', '-TSRAPL',
+            '-FZDZSN', '+TSSN', '-TSRASNPL', 'TSRAPL', 'RASN', '-SNIC', 'FZRAPL', '-FZRASNPL',
+            '+RAPL', '-RASGPL', '-TSSNPL', 'FZRASN', '+TSSNGR', 'TSPLGR', '', 'RA BR', '-TSSG',
+            '-TS', '-NA', 'NANA', '+NANA', 'NANANA', 'NANANANA']
+    true_codes = np.array([73, 61, 85, 80, 53, 63, 86, 95, 66, 71, 95, 68, 75, 45, 83, 51, 81,
+                           66, 95, 0, 79, 79, 95, 79, 65, 79, 36, 85, 97, 17, 79, 79, 80, 77,
+                           95, 77, 78, 4, 79, 95, 95, 85, 81, 95, 67, 95, 56, 97, 95, 95, 69,
+                           71, 79, 66, 79, 61, 95, 67, 97, 95, 0, 63, 17, 17, 0, 0, 0, 0, 0])
+    wx_codes = wx_code_to_numeric(data)
+    assert_array_equal(wx_codes, true_codes)
