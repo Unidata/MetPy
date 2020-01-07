e2c7acc6c78d601e3f36d35685611986a0bc839d
kgoebber <kevin.goebbert@valpo.edu>
2020-01-07 13:02:55 -0600

    add test to cover missing station information

--- tests/io/test_station_data.py
+++ tests/io/test_station_data.py
@@ -1,7 +1,8 @@
-# Copyright (c) 2008,2015,2016,2017,2018,2019,2020 MetPy Developers.
+# Copyright (c) 2020 MetPy Developers.
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
 """Test station data information."""
+import numpy as np
 from numpy.testing import assert_almost_equal
 import pandas as pd
 
@@ -10,7 +11,7 @@ from metpy.io import add_station_lat_lon
 
 def test_add_lat_lon_station_data():
     """Test for when the METAR does not correspond to a station in the dictionary."""
-    df = pd.DataFrame({'station': ['KOUN', 'KVPZ', 'KDEN']})
+    df = pd.DataFrame({'station': ['KOUN', 'KVPZ', 'KDEN', 'KAAA']})
 
     df = add_station_lat_lon(df, 'station')
     assert_almost_equal(df.loc[df.station == 'KOUN'].latitude.values, 35.25)
@@ -19,3 +20,5 @@ def test_add_lat_lon_station_data():
     assert_almost_equal(df.loc[df.station == 'KVPZ'].longitude.values, -87)
     assert_almost_equal(df.loc[df.station == 'KDEN'].latitude.values, 39.85)
     assert_almost_equal(df.loc[df.station == 'KDEN'].longitude.values, -104.65)
+    assert_almost_equal(df.loc[df.station == 'PAAA'].longitude.values, np.nan)
+    assert_almost_equal(df.loc[df.station == 'PAAA'].longitude.values, np.nan)
