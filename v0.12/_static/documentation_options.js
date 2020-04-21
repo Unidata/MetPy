e8e994b00cab6151437c8478c1bd6df5360fcdd7
Ryan May <rmay@ucar.edu>
2020-04-20 19:04:49 -0600

    Deploy 0.12.1 to GitHub Pages
    Doing this manually since we're not setup currently to deploy live docs
    from a bugfix branch.

--- v0.12/_static/documentation_options.js
+++ v0.12/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.12.1',
+    VERSION: '0.12.1.post2+gc3df3bc6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
