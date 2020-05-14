cebce5c670bf74451e11f628f6408c460088c441
Jon Thielen <github@jont.cc>
2020-05-14 13:18:46 -0500

    flake8 updates

--- src/metpy/__init__.py
+++ src/metpy/__init__.py
@@ -32,6 +32,6 @@ warnings.filterwarnings('ignore', 'numpy.dtype size changed')
 os.environ['PINT_ARRAY_PROTOCOL_FALLBACK'] = '0'
 
 from ._version import get_version  # noqa: E402
-from .xarray import *  # noqa: F401, F403
+from .xarray import *  # noqa: F401, F403, E402
 __version__ = get_version()
 del get_version
