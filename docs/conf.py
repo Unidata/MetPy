8055196f1e3d598ac3d8adf98ab9f62cbb8ac478
Ryan May <rmay@ucar.edu>
2020-11-25 01:43:00 -0700

    DOC: Update to new horizontal logo

--- docs/conf.py
+++ docs/conf.py
@@ -207,7 +207,7 @@ html_title = ' '.join((project, version))
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = os.path.join('_static', 'metpy_150x150.png')
+html_logo = os.path.join('_static', 'metpy_horizontal.png')
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
