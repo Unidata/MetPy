d8d9ae55cac6c56d3d6ad43d926e57f84838101f
Nathan Wendt <nathan.wendt@noaa.gov>
2020-12-14 16:36:57 -0600

    Added scalloped path effect
    This commit adds a path effect to make lines drawn appear to be
    scalloped. This was based off of modifying the matplotlib
    TickedStroke path effect. This style of line is often used for
    highlighting areas in SPC MCD products.
    
    Places it in metpy.plots.patheffects module with patches for
    versions of matplotlib < 3.2 in the metpy.plots._mpl module.
    
    wrong test return

--- tests/plots/test_patheffects.py
+++ tests/plots/test_patheffects.py
@@ -8,6 +8,12 @@ import pytest
 from scipy.interpolate import interp1d
 
 from metpy.plots import ColdFront, OccludedFront, StationaryFront, WarmFront
+import matplotlib.patches as patches
+from matplotlib.path import Path
+from metpy.plots import patheffects
+# Fixtures to make sure we have the right backend and consistent round
+from metpy.testing import set_agg_backend  # noqa: F401, I202
+
 
 
 @pytest.mark.mpl_image_compare(remove_text=True, tolerance=0.)
@@ -54,5 +60,35 @@ def test_stationary_spacing():
             linewidth=15)
     ax.set_ylim(0.95, 1.05)
     ax.set_xlim(-0.05, 1.05)
+
+    return fig
+
+
+@pytest.mark.mpl_image_compare(savefig_kwargs={'dpi': 300}, remove_text=True)
+def test_scalloped_stroke():
+    """Test ScallopedStroke path effect."""
+    fig = plt.figure(figsize=(9, 9))
+    ax = plt.subplot(1, 1, 1)
+
+    # test data
+    x = [-0.172, 1.078, 0.428, 0.538, 0.178,
+         -0.212, -0.482, -0.722, -0.462, -0.172]
+    y = [1.264, 0.784, -0.076, -0.846, -1.126,
+         -1.246, -1.006, 0.234, 0.754, 1.264]
+    verts = np.array([[x, y] for x, y in zip(x, y)])
+    codes = np.repeat(Path.LINETO, len(x))
+    codes[0] = Path.MOVETO
+    codes[-1] = Path.CLOSEPOLY
+
+    path = Path(verts, codes)
+    patch = patches.PathPatch(path, facecolor='#d10000', edgecolor='#000000',
+                              path_effects=[patheffects.ScallopedStroke(side='left',
+                                                                        spacing=10,
+                                                                        length=1.15)])
+
+    ax.add_patch(patch)
+    ax.axis('equal')
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
 
     return fig
