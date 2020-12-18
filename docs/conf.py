aeaac3fdfed2e5106115f025f7b4dc5a5157142a
Ryan May <rmay@ucar.edu>
2020-12-18 13:17:46 -0700

    MNT: Fix linkchecker failing on Twitter link
    Just skip it since it will not be disappearing.

--- docs/conf.py
+++ docs/conf.py
@@ -354,6 +354,8 @@ linkcheck_ignore = [r'https://www\.youtube\.com/watch\?v=[\d\w\-_]+',
     r'https://codecov.io/github/Unidata/MetPy',
     r'https://youtu\.be/[\d\w\-_]+',
     # AMS DOIs should be stable, but resolved link consistently 403's with linkcheck
-    r'https://doi.org/10.1175/.*']
+    r'https://doi.org/10.1175/.*',
+    # Giving 404s right now and is not going to change
+    r'https://twitter.com/MetPy']
 
 linkcheck_request_headers = {'https://www.ncdc.noaa.gov/': {'Accept-Encoding': 'deflate'}}
