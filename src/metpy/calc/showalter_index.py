3e39f6776ae571a11a3098465c74cb54f03f59a5
michaelavs <michaela.sizemore@gmail.com>
2021-01-27 14:23:23 -0700

    Updating to reduce variables passed into function

--- src/metpy/calc/showalter_index.py
+++ src/metpy/calc/showalter_index.py
@@ -23,25 +23,10 @@ tdc = (da[9].values + 2) * units.degC  # Dew pt temp  [C]
 ta = mpcalc.parcel_profile(p, tc[0], tdc[0])  # Parcel profile
 tac = (ta.magnitude - 273.15) * units.degC  # Parcel temp in C
 
-
-# Specify temperature and dewpt at 850mb and temp at 500mb
-t850 = da.loc[da[1] == 850]
-t850 = (t850[5].values + 2) * units.degC
-
-dpt850 = da.loc[da[1] == 850]
-dpt850 = (dpt850[9].values + 2) * units.degC
-
-t500 = da.loc[da[1] == 500]
-t500 = (t500[5].values + 2) * units.degC
-
-# Calculate lcl at the 850 hPa level
-lcl = mpcalc.lcl(850 * units.hPa, t850[0], dpt850[0])
-lcl = lcl[0]
-
 ##############################################################################
 
 
-def showalter_index(pressure, temperature, lcl850, t500): 
+def showalter_index(pressure, temperature, dewpt): 
     """Calculate Showalter Index from pressure temperature and 850 hPa lcl
     
     Showalter Index derived from [Galway1956]_:
@@ -61,18 +46,28 @@ def showalter_index(pressure, temperature, lcl850, t500):
         temperature : `pint.Quantity`
             Parcel temperature for corresponding pressure 
         
-        lcl850 (:class: `pint.Quantity`):
-            Calculated lcl taken at 850 hPa
+        dewpt (:class: `pint.Quantity`):
+            Parcel dew point temperatures for corresponding pressure
         
-        t500 (:class: ``pint.Quantity`):
-            Environmental temperature at 500 hPa 
-            
+
     Returns
     -------
     `pint.Quantity`
         Showalter Index
         
     """
+    # find the measured temperature and dew point temperature at 850 hPa.
+    idx850 = np.where(pressure == 850 * units.hPa)
+    T850 = temperature[idx850]
+    Td850 = dewpt[idx850]
+    
+    # find the parcel profile temperature at 500 hPa.
+    idx500 = np.where(pressure == 500 * units.hPa)
+    Tp500 = temperature[idx500]
+    
+    # Calculate lcl at the 850 hPa level
+    lcl = mpcalc.lcl(850 * units.hPa, T850[0], Td850[0])
+    lcl = lcl[0]
     
     # Define start and end heights for dry and moist lapse rate calculations
     p_strt = 1000 * units.hPa
@@ -80,19 +75,17 @@ def showalter_index(pressure, temperature, lcl850, t500):
     
     # Calculate parcel temp when raised dry adiabatically from surface to lcl
 
-    dl = mpcalc.dry_lapse(lcl850, tc[0], p[0])
+    dl = mpcalc.dry_lapse(lcl, tc[0], p[0])
     dl = (dl.magnitude - 273.15) * units.degC  # Change units to C
 
-    
     # Calculate parcel temp when raised moist adiabatically from lcl to 500mb
     ml = mpcalc.moist_lapse(p_end, dl, lcl)
     
     # Calculate the Showalter index
-    shox = t500 - ml
+    shox = Tp500 - ml
     return shox
 
-# Uncomment to validate the code will 'catch' if an lcl is lower than the 850mb height
-showalter = showalter_index(p, tc, lcl, t500) 
+showalter = showalter_index(p, tc, tdc) 
 
 print(showalter)
 
