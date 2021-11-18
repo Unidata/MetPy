4ca5f7ca17259187ab91379ebd98186d845535c6
Ryan May <rmay@ucar.edu>
2021-11-17 18:00:24 -0700

    DOC: Fix up call to pcolormesh
    Was failing. To avoid warnings about edges, just go ahead and
    calculation the correct quad edges.

--- examples/formats/NEXRAD_Level_2_File.py
+++ examples/formats/NEXRAD_Level_2_File.py
@@ -29,11 +29,17 @@ sweep = 0
 
 # First item in ray is header, which has azimuth angle
 az = np.array([ray[0].az_angle for ray in f.sweeps[sweep]])
+diff = np.diff(az)
+diff[diff > 180] -= 360.
+diff[diff < -180] += 360.
+avg_spacing = diff.mean()
+az = (az[:-1] + az[1:]) / 2
+az = np.concatenate(([az[0] - avg_spacing], az, [az[-1] + avg_spacing]))
 
 # 5th item is a dict mapping a var name (byte string) to a tuple
 # of (header, data array)
 ref_hdr = f.sweeps[sweep][0][4][b'REF'][0]
-ref_range = np.arange(ref_hdr.num_gates) * ref_hdr.gate_width + ref_hdr.first_gate
+ref_range = (np.arange(ref_hdr.num_gates + 1) - 0.5) * ref_hdr.gate_width + ref_hdr.first_gate
 ref = np.array([ray[4][b'REF'][1] for ray in f.sweeps[sweep]])
 
 rho_hdr = f.sweeps[sweep][0][4][b'RHO'][0]
