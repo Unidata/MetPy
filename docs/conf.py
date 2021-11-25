e44e7c7f5e2970d7ab7b2596d0011ef6bb9f3f6e
Ryan May <rmay@ucar.edu>
2021-11-25 00:02:27 -0700

    DOC: Configure some allowed redirects

--- docs/conf.py
+++ docs/conf.py
@@ -378,5 +378,9 @@ linkcheck_ignore = [r'https://www\.youtube\.com/watch\?v=[\d\w\-_]+',
     ]
 
 linkcheck_allowed_redirects = {
-    r'http://xarray\.pydata\.org/': r'http://xarray\.pydata\.org/en/stable/'
+    r'http://xarray\.pydata\.org/': r'http://xarray\.pydata\.org/en/stable/',
+    r'http://pint\.readthedocs\.io': r'https://pint\.readthedocs\.io/en/stable/',
+    r'https://conda.io/docs/': r'https://conda.io/en/latest/',
+    r'https://github.com/Unidata/MetPy/issues/new/choose': r'https://github.com/login.*choose',
+    r'https://doi.org/.*': r'https://.*'
 }
