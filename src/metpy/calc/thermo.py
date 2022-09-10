625453e53258cfc811592e527682fed5d8a8297b
Ryan May <rmay31@gmail.com>
2022-09-10 14:32:54 -0600

    DOC: Correct mixed_layer_cape_cin example

--- src/metpy/calc/thermo.py
+++ src/metpy/calc/thermo.py
@@ -2789,7 +2789,6 @@ def mixed_layer_cape_cin(pressure, temperature, dewpoint, **kwargs):
     Examples
     --------
     >>> from metpy.calc import dewpoint_from_relative_humidity, mixed_layer_cape_cin
-    >>> from metpy.calc import parcel_profile
     >>> from metpy.units import units
     >>> # pressure
     >>> p = [1008., 1000., 950., 900., 850., 800., 750., 700., 650., 600.,
@@ -2797,19 +2796,18 @@ def mixed_layer_cape_cin(pressure, temperature, dewpoint, **kwargs):
     ...      175., 150., 125., 100., 80., 70., 60., 50.,
     ...      40., 30., 25., 20.] * units.hPa
     >>> # temperature
-    >>> T = [29.3, 28.1, 23.5, 20.9, 18.4, 15.9, 13.1, 10.1, 6.7, 3.1,
+    >>> T = [29.3, 28.1, 25.5, 20.9, 18.4, 15.9, 13.1, 10.1, 6.7, 3.1,
     ...      -0.5, -4.5, -9.0, -14.8, -21.5, -29.7, -40.0, -52.4,
     ...      -59.2, -66.5, -74.1, -78.5, -76.0, -71.6, -66.7, -61.3,
     ...      -56.3, -51.7, -50.7, -47.5] * units.degC
     >>> # relative humidity
-    >>> rh = [.85, .65, .36, .39, .82, .72, .75, .86, .65, .22, .52,
+    >>> rh = [.85, .75, .56, .39, .82, .72, .75, .86, .65, .22, .52,
     ...       .66, .64, .20, .05, .75, .76, .45, .25, .48, .76, .88,
     ...       .56, .88, .39, .67, .15, .04, .94, .35] * units.dimensionless
     >>> # calculate dewpoint
     >>> Td = dewpoint_from_relative_humidity(T, rh)
-    >>> prof = parcel_profile(p, T[0], Td[0])
-    >>> mixed_layer_cape_cin(p, T, prof, depth = 50 * units.hPa)
-    (<Quantity(6003.85223, 'joule / kilogram')>, <Quantity(0, 'joule / kilogram')>)
+    >>> mixed_layer_cape_cin(p, T, Td, depth=50 * units.hPa)
+    (<Quantity(587.144138, 'joule / kilogram')>, <Quantity(-46.8016713, 'joule / kilogram')>)
 
     See Also
     --------
