13262031a6dbbaa8aea5ed66a4337877a7a06c61
akrherz <akrherz@iastate.edu>
2024-01-17 15:13:03 -0600

    perf: use package metadata before scm

--- src/metpy/_version.py
+++ src/metpy/_version.py
@@ -2,21 +2,30 @@
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
 """Tools for versioning."""
+import os
+from importlib.metadata import PackageNotFoundError, version
 
 
 def get_version():
     """Get MetPy's version.
 
-    Either get it from package metadata, or get it using version control information if
-    a development install.
+    If the package is installed (read: metpy is in site-packages), use package
+    metadata. If not, use what is provided by setuptools_scm and default to
+    package data if that also fails.
     """
+    # Inspect where this file's parent directory is located
+    moddirname = os.path.dirname(os.path.dirname(__file__))
+    # If we're in site-packages, try using package metadata
+    if moddirname.endswith('site-packages'):
+        try:
+            return version(__package__)
+        except PackageNotFoundError:
+            pass
     try:
         from setuptools_scm import get_version
         return get_version(root='../..', relative_to=__file__,
                            version_scheme='post-release')
     except (ImportError, LookupError):
-        from importlib.metadata import PackageNotFoundError, version
-
         try:
             return version(__package__)
         except PackageNotFoundError:
