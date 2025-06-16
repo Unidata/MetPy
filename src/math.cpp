8f50f44a5605aa1d38bbd06533b4a07e12568a97
Linfeng Li <linfel@Linfengs-MacBook-Air.local>
2025-06-16 09:41:54 -0600

    add reference to Lambert W function calculation

--- src/math.cpp
+++ src/math.cpp
@@ -3,12 +3,13 @@
 #include "math.hpp"
 
 double lambert_wm1(double x, double tol, int max_iter) {
+    // Corless, R.M., Gonnet, G.H., Hare, D.E.G. et al. On the LambertW function.
+    // Adv Comput Math 5, 329–359 (1996). https://doi.org/10.1007/BF02124750 
+
     if (x >= 0 || x < -1.0 / std::exp(1.0)) {
         throw std::domain_error("W₋₁(x) is only defined for -1/e < x < 0");
     }
 
-    // Initial guess for W₋₁, from Corless et al. (1996) // need to check if this is
-    // correct
     double L1 = std::log(-x);
     double L2 = std::log(-L1);
     double w = L1 - L2 + (L2 / L1);
