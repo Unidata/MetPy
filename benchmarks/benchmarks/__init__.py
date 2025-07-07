f215ac9f6a37d0ecacbc73f8d80cad11db242947
Jaye Norman <savannahnorman121@gmail.com>
2025-07-07 15:43:36 -0600

    Fixing linting

--- benchmarks/benchmarks/__init__.py
+++ benchmarks/benchmarks/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2025 MetPy Developers.
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
-""" initialization file required for ASV to run
-"""
+"""initialization file required for ASV to run."""
